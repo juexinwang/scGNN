349176a737e98680e642290e51f774fe03800809
juexinwang <juexinwang@hotmail.com>
2019-11-22 22:22:57 -0600

    add pearsonr

--- plot.py
+++ plot.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 from sklearn.metrics import roc_curve
 from sklearn.metrics import roc_auc_score
 from inspect import signature
+import scipy
 
 recon = np.load('recon.npy')
 # VIM/CDH1/ZEB1 129/68/833
@@ -14,3 +15,6 @@ plt.legend('ZEB1')
 # show the plot
 # plt.show()
 plt.savefig('result.png')
+
+scipy.stat.pearsonr(recon[:,129], recon[:,68])
+scipy.stat.spearmanr(recon[:,129], recon[:,68])
