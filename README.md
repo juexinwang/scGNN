e639eae5fb94439728282ce3d4ac58fb215b3e20
juexinwang <juexinwang@hotmail.com>
2020-01-04 16:50:04 -0600

    1.rewrite main.py 2.change main_plus.py to main_plus_bak.py

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # scGNN
+
+Reference:
+1. VAE https://github.com/pytorch/examples/tree/master/vae
+2. GAE https://github.com/tkipf/gae/tree/master/gae
--- main.py
+++ main.py
@@ -13,55 +13,46 @@ from torch import nn, optim
 from torch.nn import functional as F
 from sklearn.decomposition import PCA
 from sklearn.metrics import silhouette_samples, silhouette_score
-
 from model import AE, VAE, VAE2d
 from util_function import *
 from graph_function import *
 
-parser = argparse.ArgumentParser(description='AutoEncoder-EM for scRNA')
+parser = argparse.ArgumentParser(description='Regularized EM AutoEncoder for scRNA')
 parser.add_argument('--datasetName', type=str, default='MMPbasal',
                     help='TGFb/sci-CAR/sci-CAR_LTMG/2.Yan/5.Pollen/MPPbasal/MPPbasal_all/MPPbasal_allgene/MPPbasal_allcell/MPPepo/MMPbasal_LTMG/MMPbasal_all_LTMG')
 parser.add_argument('--batch-size', type=int, default=10000, metavar='N',
                     help='input batch size for training (default: 128)')
-parser.add_argument('--total-epochs', type=int, default=3, metavar='N',
-                    help='number of epochs in total iteration (default: 3)')
+parser.add_argument('--EM-epochs', type=int, default=3, metavar='N',
+                    help='number of epochs in EM iteration (default: 3)')
 parser.add_argument('--epochs', type=int, default=500, metavar='N',
                     help='number of epochs to train (default: 10)')
 parser.add_argument('--no-cuda', action='store_true', default=True,
                     help='enables CUDA training')
 parser.add_argument('--seed', type=int, default=1, metavar='S',
                     help='random seed (default: 1)')
-parser.add_argument('--regulized-type', type=str, default='noregu',
-                    help='regulized type (default: Graph), otherwise: noregu')
+parser.add_argument('--regulized-type', type=str, default='Graph',
+                    help='regulized type (default: Graph) in EM, otherwise: noregu')
 parser.add_argument('--discreteTag', type=bool, default=False,
-                    help='False/True')
+                    help='whether input is raw or 0/1 (default: False)')
 parser.add_argument('--k', type=int, default=10,
-                    help='k in KNN graph (default: 10)')
+                    help='parameter k in KNN graph (default: 10)')
 parser.add_argument('--knn-distance', type=str, default='euclidean',
                     help='KNN graph distance type (default: euclidean)')                    
 parser.add_argument('--model', type=str, default='AE',
-                    help='VAE/AE')
-parser.add_argument('--npyDir', type=str, default='npynzGraph/',
+                    help='VAE/AE (default: AE)')
+parser.add_argument('--npyDir', type=str, default='npyGraph/',
                     help='save npy results in directory')
-parser.add_argument('--zerofillFlag', type=bool, default=True,
-                    help='fill zero or not in the iteration')
+parser.add_argument('--zerofillFlag', type=bool, default=False,
+                    help='fill zero or not before EM process (default: False)')
 parser.add_argument('--log-interval', type=int, default=100, metavar='N',
                     help='how many batches to wait before logging training status')
 args = parser.parse_args()
 args.cuda = not args.no_cuda and torch.cuda.is_available()
 
 torch.manual_seed(args.seed)
-
 device = torch.device("cuda" if args.cuda else "cpu")
 
 kwargs = {'num_workers': 1, 'pin_memory': True} if args.cuda else {}
-# train_loader = torch.utils.data.DataLoader(
-#     datasets.MNIST('../data', train=True, download=True,
-#                    transform=transforms.ToTensor()),
-#     batch_size=args.batch_size, shuffle=True, **kwargs)
-# test_loader = torch.utils.data.DataLoader(
-#     datasets.MNIST('../data', train=False, transform=transforms.ToTensor()),
-#     batch_size=args.batch_size, shuffle=True, **kwargs)
 
 scData = scDataset(args.datasetName, args.discreteTag)
 train_loader = DataLoader(scData, batch_size=args.batch_size, shuffle=True, **kwargs)
@@ -74,8 +65,13 @@ elif args.model == 'AE':
     model = AE(dim=scData.features.shape[1]).to(device)
 optimizer = optim.Adam(model.parameters(), lr=1e-3)
 
-#TODO: batch needs to implement
-def train(epoch, train_loader=train_loader, forceReguFlag=False):
+#TODO: needs to implement batch
+def train(epoch, train_loader=train_loader, EMFlag=False):
+    '''
+    EMFlag indicates whether in EM processes. 
+        If in EM, use regulized-type parsed from program entrance,
+        Otherwise, noregu
+    '''
     model.train()
     train_loss = 0
     # for batch_idx, (data, _) in enumerate(train_loader):
@@ -87,20 +83,22 @@ def train(epoch, train_loader=train_loader, forceReguFlag=False):
             recon_batch, mu, logvar, z = model(data)
             # Original
             # loss = loss_function(recon_batch, data, mu, logvar)
-            if forceReguFlag:
-                loss = loss_function_graph(recon_batch, data.view(-1, recon_batch.shape[1]), mu, logvar, adjsample, adjfeature, 'Graph', args.model)
-            else:
+            if EMFlag:
                 loss = loss_function_graph(recon_batch, data.view(-1, recon_batch.shape[1]), mu, logvar, adjsample, adjfeature, args.regulized_type, args.model)
+            else:
+                loss = loss_function_graph(recon_batch, data.view(-1, recon_batch.shape[1]), mu, logvar, adjsample, adjfeature, 'noregu', args.model)
+            
         elif args.model == 'AE':
             recon_batch, z = model(data)
             mu_dummy = ''
             logvar_dummy = ''
             # Original
             # loss = loss_function(recon_batch, data, mu, logvar)
-            if forceReguFlag:
-                loss = loss_function_graph(recon_batch, data.view(-1, recon_batch.shape[1]), mu_dummy, logvar_dummy, adjsample, adjfeature, 'Graph', args.model)
-            else:
+            if EMFlag:
                 loss = loss_function_graph(recon_batch, data.view(-1, recon_batch.shape[1]), mu_dummy, logvar_dummy, adjsample, adjfeature, args.regulized_type, args.model)
+            else:
+                loss = loss_function_graph(recon_batch, data.view(-1, recon_batch.shape[1]), mu_dummy, logvar_dummy, adjsample, adjfeature, 'noregu', args.model)
+               
         loss.backward()
         train_loss += loss.item()
         optimizer.step()
@@ -115,56 +113,20 @@ def train(epoch, train_loader=train_loader, forceReguFlag=False):
 
     return recon_batch, data, z
 
-# TODO
-def test(epoch):
-    model.eval()
-    test_loss = 0
-    with torch.no_grad():
-        for i, (data, _) in enumerate(test_loader):
-            data = data.to(device)
-            recon_batch, mu, logvar = model(data)
-            test_loss += loss_function(recon_batch, data, mu, logvar).item()
-            if i == 0:
-                n = min(data.size(0), 8)
-                comparison = torch.cat([data[:n],
-                                      recon_batch.view(args.batch_size, 1, 28, 28)[:n]])
-                save_image(comparison.cpu(),
-                         'results/reconstruction_' + str(epoch) + '.png', nrow=n)
-
-    test_loss /= len(test_loader.dataset)
-    print('====> Test set loss: {:.4f}'.format(test_loss))
-
 if __name__ == "__main__":
     discreteStr = ''
     if args.discreteTag:
-        discreteStr = 'D'
-    # pca = PCA(n_components=100)
-    # pca_result = pca.fit_transform(scData.features.todense())
-    # # adj, edgeList = generateAdj(scData.features, graphType='KNNgraph', para = 'cosine:5')
-    # # adj, edgeList = generateAdj(pca_result, graphType='KNNgraphPairwise', para = 'Pairwise:10')
-    # # adj, edgeList = generateAdj(pca_result, graphType='KNNgraphThreshold', para = 'cosine:10:0.5')
-    # adj, edgeList = generateAdj(pca_result, graphType='KNNgraphML', para = 'euclidean:10')
-    # adjdense = sp.csr_matrix.todense(adj)
-    # adjsample = torch.from_numpy(adjdense)
-    # adjsample = adjsample.type(torch.FloatTensor)
-    # np.save(args.datasetName+'_'+args.regulized_type+discreteStr+'_edgeList_init.npy',edgeList)       
+        discreteStr = 'D'       
+    # adjsample refer to cell-cell regulization, now we only have adjsample
     adjsample = None
+    # adjfeature refer to gene-gene regulization
     adjfeature = None
     for epoch in range(1, args.epochs + 1):
-        recon, original, z = train(epoch, forceReguFlag=False)
-        # test(epoch)
-        # with torch.no_grad():
-        #     sample = torch.randn(64, 20).to(device)
-        #     sample = model.decode(sample).cpu()
-        #     save_image(sample.view(64, 1, 28, 28),
-        #                'results/sample_' + str(epoch) + '.png')
-    
+        recon, original, z = train(epoch, EMFlag=False)
     
     reconOut = recon.detach().cpu().numpy()
-    # originalOut = original.detach().cpu().numpy()
     zOut = z.detach().cpu().numpy()   
     np.save(args.npyDir+args.datasetName+'_'+args.regulized_type+discreteStr+'_recon.npy',reconOut)
-    # np.save(args.npyDir+args.datasetName+'_'+args.regulized_type+discreteStr+'_original.npy',originalOut)
     np.save(args.npyDir+args.datasetName+'_'+args.regulized_type+discreteStr+'_z.npy',zOut)
 
     # Here para = 'euclidean:10'
@@ -172,35 +134,32 @@ if __name__ == "__main__":
     adjdense = sp.csr_matrix.todense(adj)
     adjsample = torch.from_numpy(adjdense)
 
-    np.save(args.npyDir+args.datasetName+'_'+args.regulized_type+discreteStr+'init_edgeList.npy',edgeList)
+    np.save(args.npyDir+args.datasetName+'_'+args.regulized_type+discreteStr+'_init_edgeList.npy',edgeList)
 
-
-    #if fill the zero in the iteration
-    start_time = time.time()
+    #Fill the zeros before EM iteration
     # TODO: better implementation
     if args.zerofillFlag:
+        # start_time = time.time()
         for nz_index in range(len(scData.nz_i)):
             # tmp = scipy.sparse.lil_matrix.todense(scData.features[scData.nz_i[nz_index], scData.nz_j[nz_index]])
             # tmp = np.asarray(tmp).reshape(-1)[0]
             tmp = scData.features[scData.nz_i[nz_index], scData.nz_j[nz_index]]
             reconOut[scData.nz_i[nz_index], scData.nz_j[nz_index]] = tmp
         recon = reconOut
-    print("--- %s seconds ---" % (time.time() - start_time))
+        # print("--- %s seconds ---" % (time.time() - start_time))
 
-    for bigepoch in range(0, args.total_epochs):
+    print("EM processes started")
+    for bigepoch in range(0, args.EM_epochs):
+        # Use new dataloader
         scDataInter = scDatasetInter(recon)
         train_loader = DataLoader(scDataInter, batch_size=args.batch_size, shuffle=True, **kwargs)
 
         for epoch in range(1, args.epochs + 1):
-            # recon, original, z = train(epoch, forceReguFlag=True, tensorType=False)
-            recon, original, z = train(epoch, forceReguFlag=True)
+            recon, original, z = train(epoch, EMFlag=True)
         
         reconOut = recon.detach().cpu().numpy()
-        # originalOut = original.detach().cpu().numpy()
         zOut = z.detach().cpu().numpy()
-
         np.save(args.npyDir+args.datasetName+'_'+args.regulized_type+discreteStr+'_recon'+str(bigepoch)+'.npy',reconOut)
-        # np.save(args.npyDir+args.datasetName+'_'+args.regulized_type+discreteStr+'_original'+str(bigepoch)+'.npy',originalOut)
         np.save(args.npyDir+args.datasetName+'_'+args.regulized_type+discreteStr+'_z'+str(bigepoch)+'.npy',zOut)
 
         # Here para = 'euclidean:10'
@@ -208,5 +167,5 @@ if __name__ == "__main__":
         adjdense = sp.csr_matrix.todense(adj)
         adjsample = torch.from_numpy(adjdense)
 
-    np.save(args.npyDir+args.datasetName+'_'+args.regulized_type+discreteStr+'_edgeList_final.npy',edgeList)
+    np.save(args.npyDir+args.datasetName+'_'+args.regulized_type+discreteStr+'_final_edgeList.npy',edgeList)
 
