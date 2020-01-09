7cbe72cc09e405e6fefab458d88f034cdfd97229
juexinwang <juexinwang@hotmail.com>
2020-01-08 22:57:59 -0600

    change sh for evaluation

--- results/results_experimentImpute_1_g_e.sh
+++ results/results_experimentImpute_1_g_e.sh
@@ -1,10 +1,25 @@
 python3 -W ignore results_impute.py --datasetName MMPbasal --npyDir ../npyImputeG1E/
-python3 -W ignore results_impute.py --datasetName MMPbasal --imputeMode --npyDir ../npyImputeG1E/
+python3 -W ignore results_impute.py --datasetName MMPbasal --discreteTag  --npyDir ../npyImputeG1E/
 python3 -W ignore results_impute.py --datasetName MMPbasal_LTMG --npyDir ../npyImputeG1E/
 
 python3 -W ignore results_impute.py --datasetName MMPbasal_all --npyDir ../npyImputeG1E/
-python3 -W ignore results_impute.py --datasetName MMPbasal_all --npyDir ../npyImputeG1E/
+python3 -W ignore results_impute.py --datasetName MMPbasal_all --discreteTag --npyDir ../npyImputeG1E/
 python3 -W ignore results_impute.py --datasetName MMPbasal_all_LTMG --npyDir ../npyImputeG1E/
 
 python3 -W ignore results_impute.py --datasetName MMPbasal_allcell --npyDir ../npyImputeG1E/
-python3 -W ignore results_impute.py --datasetName MMPbasal_allcell --npyDir ../npyImputeG1E/
+python3 -W ignore results_impute.py --datasetName MMPbasal_allcell --discreteTag  --npyDir ../npyImputeG1E/
+
+
+for i in {0..2}
+do
+    python3 -W ignore results_impute.py --datasetName MMPbasal --reconstr $i --npyDir ../npyImputeG1E/
+    python3 -W ignore results_impute.py --datasetName MMPbasal --discreteTag  --reconstr $i --npyDir ../npyImputeG1E/
+    python3 -W ignore results_impute.py --datasetName MMPbasal_LTMG --reconstr $i --npyDir ../npyImputeG1E/
+
+    python3 -W ignore results_impute.py --datasetName MMPbasal_all --reconstr $i --npyDir ../npyImputeG1E/
+    python3 -W ignore results_impute.py --datasetName MMPbasal_all --discreteTag --reconstr $i --npyDir ../npyImputeG1E/
+    python3 -W ignore results_impute.py --datasetName MMPbasal_all_LTMG --reconstr $i --npyDir ../npyImputeG1E/
+
+    python3 -W ignore results_impute.py --datasetName MMPbasal_allcell --reconstr $i --npyDir ../npyImputeG1E/
+    python3 -W ignore results_impute.py --datasetName MMPbasal_allcell --discreteTag  --reconstr $i --npyDir ../npyImputeG1E/
+done
--- results/results_experimentImpute_1_g_f.sh
+++ results/results_experimentImpute_1_g_f.sh
@@ -1,10 +1,24 @@
-python3 -W ignore main.py --datasetName MMPbasal --EMtype EM --imputeMode  --npyDir npyImputeG1F/
-python3 -W ignore main.py --datasetName MMPbasal --discreteTag --EMtype EM --imputeMode  --npyDir npyImputeG1F/
-python3 -W ignore main.py --datasetName MMPbasal_LTMG --EMtype EM --imputeMode  --npyDir npyImputeG1F/
+python3 -W ignore main.py --datasetName MMPbasal   --npyDir ../npyImputeG1F/
+python3 -W ignore main.py --datasetName MMPbasal --discreteTag   --npyDir ../npyImputeG1F/
+python3 -W ignore main.py --datasetName MMPbasal_LTMG   --npyDir ../npyImputeG1F/
 
-python3 -W ignore main.py --datasetName MMPbasal_all --EMtype EM --imputeMode  --npyDir npyImputeG1F/
-python3 -W ignore main.py --datasetName MMPbasal_all --discreteTag --EMtype EM --imputeMode  --npyDir npyImputeG1F/
-python3 -W ignore main.py --datasetName MMPbasal_all_LTMG --EMtype EM --imputeMode  --npyDir npyImputeG1F/
+python3 -W ignore main.py --datasetName MMPbasal_all   --npyDir ../npyImputeG1F/
+python3 -W ignore main.py --datasetName MMPbasal_all --discreteTag   --npyDir ../npyImputeG1F/
+python3 -W ignore main.py --datasetName MMPbasal_all_LTMG   --npyDir ../npyImputeG1F/
 
-python3 -W ignore main.py --datasetName MMPbasal_allcell --EMtype EM --imputeMode  --npyDir npyImputeG1F/
-python3 -W ignore main.py --datasetName MMPbasal_allcell --discreteTag --EMtype EM --imputeMode  --npyDir npyImputeG1F/
+python3 -W ignore main.py --datasetName MMPbasal_allcell   --npyDir ../npyImputeG1F/
+python3 -W ignore main.py --datasetName MMPbasal_allcell --discreteTag   --npyDir ../npyImputeG1F/
+
+for i in {0..2}
+do
+    python3 -W ignore main.py --datasetName MMPbasal --reconstr $i  --npyDir ../npyImputeG1F/
+    python3 -W ignore main.py --datasetName MMPbasal --discreteTag --reconstr $i  --npyDir ../npyImputeG1F/
+    python3 -W ignore main.py --datasetName MMPbasal_LTMG --reconstr $i  --npyDir ../npyImputeG1F/
+
+    python3 -W ignore main.py --datasetName MMPbasal_all --reconstr $i  --npyDir ../npyImputeG1F/
+    python3 -W ignore main.py --datasetName MMPbasal_all --discreteTag --reconstr $i  --npyDir ../npyImputeG1F/
+    python3 -W ignore main.py --datasetName MMPbasal_all_LTMG --reconstr $i  --npyDir ../npyImputeG1F/
+
+    python3 -W ignore main.py --datasetName MMPbasal_allcell --reconstr $i  --npyDir ../npyImputeG1F/
+    python3 -W ignore main.py --datasetName MMPbasal_allcell --discreteTag --reconstr $i  --npyDir ../npyImputeG1F/
+done
