60fe02c99aa5c773affd091ab16aeb52168080be
Wang <wangjue@umsystem.edu>
2020-01-11 19:00:44 -0600

    Add new command for clustering results

---
+++ run_experiment_1_g_e_6.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+######################### Batch Headers #########################
+#SBATCH -p Lewis                    # use the Lewis partition
+#SBATCH -J eI1ge_6                  # give the job a custom name
+#SBATCH -o results-%j.out           # give the job output a custom name
+#SBATCH -t 2-00:00                  # two hour time limit
+#SBATCH -N 1                        # number of nodes
+#SBATCH -n 4                        # number of cores (AKA tasks)
+#################################################################
+
+module load miniconda3
+source activate my_environment
+
+# Now you can run Python scripts that use the packages in your environment
+python3 -W ignore main.py --datasetName MMPbasal_all_LTMG --EMtype EM --useGAEembedding  --imputeMode  --npyDir npyImputeG1E/
