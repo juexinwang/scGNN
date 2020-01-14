f9eb0ddc574956129c49fa66eefd0c40628fd044
juexinwang <juexinwang@hotmail.com>
2020-01-13 22:27:26 -0600

    change python3 to python in GPU workstation

--- results/results_experimentImpute_2_n_e.sh
+++ results/results_experimentImpute_2_n_e.sh
@@ -1,32 +1,32 @@
-python3 -W ignore results_impute.py --datasetName MMPbasal --regulized-type noregu  --npyDir ../npyImputeN2E/
-python3 -W ignore results_impute.py --datasetName MMPbasal --discreteTag  --regulized-type noregu  --npyDir ../npyImputeN2E/
-python3 -W ignore results_impute.py --datasetName MMPbasal_LTMG --regulized-type noregu  --npyDir ../npyImputeN2E/
+python -W ignore results_impute.py --datasetName MMPbasal --regulized-type noregu  --npyDir ../npyImputeN2E/
+python -W ignore results_impute.py --datasetName MMPbasal --discreteTag  --regulized-type noregu  --npyDir ../npyImputeN2E/
+python -W ignore results_impute.py --datasetName MMPbasal_LTMG --regulized-type noregu  --npyDir ../npyImputeN2E/
 
-python3 -W ignore results_impute.py --datasetName MMPbasal_all --regulized-type noregu  --npyDir ../npyImputeN2E/
-python3 -W ignore results_impute.py --datasetName MMPbasal_all --discreteTag  --regulized-type noregu  --npyDir ../npyImputeN2E/
-python3 -W ignore results_impute.py --datasetName MMPbasal_all_LTMG --regulized-type noregu  --npyDir ../npyImputeN2E/
+python -W ignore results_impute.py --datasetName MMPbasal_all --regulized-type noregu  --npyDir ../npyImputeN2E/
+python -W ignore results_impute.py --datasetName MMPbasal_all --discreteTag  --regulized-type noregu  --npyDir ../npyImputeN2E/
+python -W ignore results_impute.py --datasetName MMPbasal_all_LTMG --regulized-type noregu  --npyDir ../npyImputeN2E/
 
-python3 -W ignore results_impute.py --datasetName MMPbasal_allcell --regulized-type noregu  --npyDir ../npyImputeN2E/
-python3 -W ignore results_impute.py --datasetName MMPbasal_allcell --discreteTag  --regulized-type noregu  --npyDir ../npyImputeN2E/
+python -W ignore results_impute.py --datasetName MMPbasal_allcell --regulized-type noregu  --npyDir ../npyImputeN2E/
+python -W ignore results_impute.py --datasetName MMPbasal_allcell --discreteTag  --regulized-type noregu  --npyDir ../npyImputeN2E/
 
-python3 -W ignore results_impute.py --datasetName MMPbasal_2000 --regulized-type noregu  --npyDir ../npyImputeN2E/
-python3 -W ignore results_impute.py --datasetName MMPbasal_2000 --discreteTag  --regulized-type noregu  --npyDir ../npyImputeN2E/
-python3 -W ignore results_impute.py --datasetName MMPbasal_2000_LTMG --regulized-type noregu  --npyDir ../npyImputeN2E/
+python -W ignore results_impute.py --datasetName MMPbasal_2000 --regulized-type noregu  --npyDir ../npyImputeN2E/
+python -W ignore results_impute.py --datasetName MMPbasal_2000 --discreteTag  --regulized-type noregu  --npyDir ../npyImputeN2E/
+python -W ignore results_impute.py --datasetName MMPbasal_2000_LTMG --regulized-type noregu  --npyDir ../npyImputeN2E/
 
 for i in {0..4}
 do
-    python3 -W ignore results_impute.py --datasetName MMPbasal --regulized-type noregu --reconstr $i --npyDir ../npyImputeN2E/
-    python3 -W ignore results_impute.py --datasetName MMPbasal --discreteTag  --regulized-type noregu --reconstr $i --npyDir ../npyImputeN2E/
-    python3 -W ignore results_impute.py --datasetName MMPbasal_LTMG --regulized-type noregu --reconstr $i --npyDir ../npyImputeN2E/
+    python -W ignore results_impute.py --datasetName MMPbasal --regulized-type noregu --reconstr $i --npyDir ../npyImputeN2E/
+    python -W ignore results_impute.py --datasetName MMPbasal --discreteTag  --regulized-type noregu --reconstr $i --npyDir ../npyImputeN2E/
+    python -W ignore results_impute.py --datasetName MMPbasal_LTMG --regulized-type noregu --reconstr $i --npyDir ../npyImputeN2E/
 
-    python3 -W ignore results_impute.py --datasetName MMPbasal_all --regulized-type noregu --reconstr $i --npyDir ../npyImputeN2E/
-    python3 -W ignore results_impute.py --datasetName MMPbasal_all --discreteTag  --regulized-type noregu --reconstr $i --npyDir ../npyImputeN2E/
-    python3 -W ignore results_impute.py --datasetName MMPbasal_all_LTMG --regulized-type noregu --reconstr $i --npyDir ../npyImputeN2E/
+    python -W ignore results_impute.py --datasetName MMPbasal_all --regulized-type noregu --reconstr $i --npyDir ../npyImputeN2E/
+    python -W ignore results_impute.py --datasetName MMPbasal_all --discreteTag  --regulized-type noregu --reconstr $i --npyDir ../npyImputeN2E/
+    python -W ignore results_impute.py --datasetName MMPbasal_all_LTMG --regulized-type noregu --reconstr $i --npyDir ../npyImputeN2E/
 
-    python3 -W ignore results_impute.py --datasetName MMPbasal_allcell --regulized-type noregu --reconstr $i --npyDir ../npyImputeN2E/
-    python3 -W ignore results_impute.py --datasetName MMPbasal_allcell --discreteTag  --regulized-type noregu --reconstr $i --npyDir ../npyImputeN2E/
+    python -W ignore results_impute.py --datasetName MMPbasal_allcell --regulized-type noregu --reconstr $i --npyDir ../npyImputeN2E/
+    python -W ignore results_impute.py --datasetName MMPbasal_allcell --discreteTag  --regulized-type noregu --reconstr $i --npyDir ../npyImputeN2E/
 
-    python3 -W ignore results_impute.py --datasetName MMPbasal_2000 --regulized-type noregu --reconstr $i --npyDir ../npyImputeN2E/
-    python3 -W ignore results_impute.py --datasetName MMPbasal_2000 --discreteTag  --regulized-type noregu --reconstr $i --npyDir ../npyImputeN2E/
-    python3 -W ignore results_impute.py --datasetName MMPbasal_2000_LTMG --regulized-type noregu --reconstr $i --npyDir ../npyImputeN2E/
+    python -W ignore results_impute.py --datasetName MMPbasal_2000 --regulized-type noregu --reconstr $i --npyDir ../npyImputeN2E/
+    python -W ignore results_impute.py --datasetName MMPbasal_2000 --discreteTag  --regulized-type noregu --reconstr $i --npyDir ../npyImputeN2E/
+    python -W ignore results_impute.py --datasetName MMPbasal_2000_LTMG --regulized-type noregu --reconstr $i --npyDir ../npyImputeN2E/
 done
