4f6beb0e65b9a28b0ada93b8e2b25569294b242e
juexinwang <juexinwang@hotmail.com>
2020-01-16 21:23:52 -0600

    add celltype 2_g_e

--- submitCluster_celltype_R.sh
+++ submitCluster_celltype_R.sh
@@ -9,3 +9,15 @@ do
 sbatch run_experiment_2_n_f_$i.sh
 sleep 3
 done
+
+for i in {9..25}
+do
+sbatch run_experiment_2_g_e_$i.sh
+sleep 3
+done
+
+for i in {9..25}
+do
+sbatch run_experiment_2_g_f_$i.sh
+sleep 3
+done
