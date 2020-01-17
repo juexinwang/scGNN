56d86ac6bc67100bf2c49f5a4cff7b106141ac82
juexinwang <juexinwang@hotmail.com>
2020-01-17 12:46:28 -0600

    move results script generating to .sh

--- results/submitCluster_Result_Celltype.sh
+++ results/submitCluster_Result_Celltype.sh
@@ -1,5 +1,11 @@
 for i in {0..27}
 do
+python results_Reading.py --methodName $i > run_Results_Celltype_$i.sh
+done
+
+# submit
+for i in {0..27}
+do
 sbatch run_Results_Celltype_$i.sh
 sleep 3
 done
--- results/submitCluster_Result_Impute.sh
+++ results/submitCluster_Result_Impute.sh
@@ -1,5 +1,11 @@
 for i in {0..27}
 do
+python results_Reading.py --methodName $i --imputeMode > run_Results_Impute_$i.sh
+done
+
+#submit
+for i in {0..27}
+do
 sbatch run_Results_Impute_$i.sh
 sleep 3
 done
