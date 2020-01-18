06da1a583e902a6a95a266d9962023194bcf8ddb
juexinwang <juexinwang@hotmail.com>
2020-01-18 14:16:27 -0600

    Change name of Other Results

--- results/Other_Result.sh
+++ results/Other_Result.sh
@@ -3,6 +3,7 @@ do
 python results_Reading.py --methodName $i --runMode > OtherResults_$i.txt
 done
 
+echo 'Start Impute:\n'
 for i in {0..0}
 do
 python results_Reading.py --methodName $i --imputeMode --runMode > OtherResults_Impute_$i.txt
