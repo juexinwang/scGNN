ca5252b9b62bfe316f564ef18a6525a992f40499
juexinwang <juexinwang@hotmail.com>
2020-01-18 14:48:35 -0600

    Change results for other methods

---
+++ results/Other_Result_Evaluation.sh
@@ -0,0 +1,10 @@
+for i in {0..0}
+do
+python Other_results_Reading.py --methodName $i --runMode > OtherResults_$i.txt
+done
+
+echo 'Start Impute:\n'
+for i in {0..0}
+do
+python Other_results_Reading.py --methodName $i --imputeMode --runMode > OtherResults_Impute_$i.txt
+done
