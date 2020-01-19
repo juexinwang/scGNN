1ed210498931384107378b9c475beeff01613338
juexinwang <juexinwang@hotmail.com>
2020-01-18 19:26:04 -0600

    change shell to cluster mode

--- results/Other_Result_Evaluation.sh
+++ results/Other_Result_Evaluation.sh
@@ -1,11 +1,11 @@
 for i in {0..0}
 do
-python Other_results_Reading.py --methodName $i --runMode > OtherResults_$i.txt
+python Other_results_Reading.py --methodName $i > runOtherResults_$i.sh
 done
 
 echo 'Start Impute:\n'
 for i in {0..1}
 do
 # python Other_results_Reading.py --methodName $i --imputeMode --runMode > OtherResults_Impute_$i.txt
-python Other_results_Reading.py --methodName $i --imputeMode > runResults_Impute_$i.sh
+python Other_results_Reading.py --methodName $i --imputeMode > runOtherResults_Impute_$i.sh
 done
