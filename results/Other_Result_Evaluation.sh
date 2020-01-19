05d0b6b9bf9d4deceafd246bfe6c81ff633d702d
juexinwang <juexinwang@hotmail.com>
2020-01-18 19:16:33 -0600

    Add MAGIC results

--- results/Other_Result_Evaluation.sh
+++ results/Other_Result_Evaluation.sh
@@ -1,10 +1,10 @@
-# for i in {0..0}
-# do
-# python Other_results_Reading.py --methodName $i --runMode > OtherResults_$i.txt
-# done
+for i in {0..0}
+do
+python Other_results_Reading.py --methodName $i --runMode > OtherResults_$i.txt
+done
 
 echo 'Start Impute:\n'
-for i in {1..1}
+for i in {0..1}
 do
 # python Other_results_Reading.py --methodName $i --imputeMode --runMode > OtherResults_Impute_$i.txt
 python Other_results_Reading.py --methodName $i --imputeMode > runResults_Impute_$i.sh
