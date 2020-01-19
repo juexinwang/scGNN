f7a5f5c55f861bf69576295863f842fff0a9e49b
Wang <wangjue@umsystem.edu>
2020-01-19 08:41:05 -0600

    Fix error in shell of G2E and N2E, G2F and N2F

--- experimentImpute_2_n_f_KMeans.sh
+++ experimentImpute_2_n_f_KMeans.sh
@@ -1,34 +1,34 @@
-python3 -W ignore main.py --datasetName MMPbasal --regulized-type noregu --EMtype celltypeEM  --clustering-method KMeans --imputeMode --npyDir npyImputeG2F_KMeans/
-python3 -W ignore main.py --datasetName MMPbasal --discreteTag --regulized-type noregu --EMtype celltypeEM  --clustering-method KMeans --imputeMode --npyDir npyImputeG2F_KMeans/
-python3 -W ignore main.py --datasetName MMPbasal_LTMG --regulized-type noregu --EMtype celltypeEM  --clustering-method KMeans --imputeMode --npyDir npyImputeG2F_KMeans/
+python3 -W ignore main.py --datasetName MMPbasal --regulized-type noregu --EMtype celltypeEM  --clustering-method KMeans --imputeMode --npyDir npyImputeN2F_KMeans/
+python3 -W ignore main.py --datasetName MMPbasal --discreteTag --regulized-type noregu --EMtype celltypeEM  --clustering-method KMeans --imputeMode --npyDir npyImputeN2F_KMeans/
+python3 -W ignore main.py --datasetName MMPbasal_LTMG --regulized-type noregu --EMtype celltypeEM  --clustering-method KMeans --imputeMode --npyDir npyImputeN2F_KMeans/
 
-python3 -W ignore main.py --datasetName MMPbasal_all --regulized-type noregu --EMtype celltypeEM  --clustering-method KMeans --imputeMode --npyDir npyImputeG2F_KMeans/
-python3 -W ignore main.py --datasetName MMPbasal_all --discreteTag --regulized-type noregu --EMtype celltypeEM  --clustering-method KMeans --imputeMode --npyDir npyImputeG2F_KMeans/
-python3 -W ignore main.py --datasetName MMPbasal_all_LTMG --regulized-type noregu --EMtype celltypeEM  --clustering-method KMeans --imputeMode --npyDir npyImputeG2F_KMeans/
+python3 -W ignore main.py --datasetName MMPbasal_all --regulized-type noregu --EMtype celltypeEM  --clustering-method KMeans --imputeMode --npyDir npyImputeN2F_KMeans/
+python3 -W ignore main.py --datasetName MMPbasal_all --discreteTag --regulized-type noregu --EMtype celltypeEM  --clustering-method KMeans --imputeMode --npyDir npyImputeN2F_KMeans/
+python3 -W ignore main.py --datasetName MMPbasal_all_LTMG --regulized-type noregu --EMtype celltypeEM  --clustering-method KMeans --imputeMode --npyDir npyImputeN2F_KMeans/
 
-python3 -W ignore main.py --datasetName MMPbasal_allcell --regulized-type noregu --EMtype celltypeEM  --clustering-method KMeans --imputeMode --npyDir npyImputeG2F_KMeans/
-python3 -W ignore main.py --datasetName MMPbasal_allcell --discreteTag --regulized-type noregu --EMtype celltypeEM  --clustering-method KMeans --imputeMode --npyDir npyImputeG2F_KMeans/
+python3 -W ignore main.py --datasetName MMPbasal_allcell --regulized-type noregu --EMtype celltypeEM  --clustering-method KMeans --imputeMode --npyDir npyImputeN2F_KMeans/
+python3 -W ignore main.py --datasetName MMPbasal_allcell --discreteTag --regulized-type noregu --EMtype celltypeEM  --clustering-method KMeans --imputeMode --npyDir npyImputeN2F_KMeans/
 
-python3 -W ignore main.py --datasetName MMPbasal_2000 --regulized-type noregu --EMtype celltypeEM  --clustering-method KMeans --imputeMode --npyDir npyImputeG2F_KMeans/
-python3 -W ignore main.py --datasetName MMPbasal_2000 --discreteTag --regulized-type noregu --EMtype celltypeEM  --clustering-method KMeans --imputeMode --npyDir npyImputeG2F_KMeans/
-python3 -W ignore main.py --datasetName MMPbasal_2000_LTMG --regulized-type noregu --EMtype celltypeEM  --clustering-method KMeans --imputeMode --npyDir npyImputeG2F_KMeans/
+python3 -W ignore main.py --datasetName MMPbasal_2000 --regulized-type noregu --EMtype celltypeEM  --clustering-method KMeans --imputeMode --npyDir npyImputeN2F_KMeans/
+python3 -W ignore main.py --datasetName MMPbasal_2000 --discreteTag --regulized-type noregu --EMtype celltypeEM  --clustering-method KMeans --imputeMode --npyDir npyImputeN2F_KMeans/
+python3 -W ignore main.py --datasetName MMPbasal_2000_LTMG --regulized-type noregu --EMtype celltypeEM  --clustering-method KMeans --imputeMode --npyDir npyImputeN2F_KMeans/
 
-python3 -W ignore main.py --datasetName 4.Yan --n-clusters 7 --regulized-type noregu --EMtype celltypeEM  --clustering-method KMeans --imputeMode --npyDir npyImputeG2F_KMeans/
-python3 -W ignore main.py --datasetName 4.Yan --n-clusters 7 --discreteTag --regulized-type noregu --EMtype celltypeEM  --clustering-method KMeans --imputeMode --npyDir npyImputeG2F_KMeans/
-python3 -W ignore main.py --datasetName 4.Yan_LTMG --n-clusters 7 --regulized-type noregu --EMtype celltypeEM  --clustering-method KMeans --imputeMode --npyDir npyImputeG2F_KMeans/
+python3 -W ignore main.py --datasetName 4.Yan --n-clusters 7 --regulized-type noregu --EMtype celltypeEM  --clustering-method KMeans --imputeMode --npyDir npyImputeN2F_KMeans/
+python3 -W ignore main.py --datasetName 4.Yan --n-clusters 7 --discreteTag --regulized-type noregu --EMtype celltypeEM  --clustering-method KMeans --imputeMode --npyDir npyImputeN2F_KMeans/
+python3 -W ignore main.py --datasetName 4.Yan_LTMG --n-clusters 7 --regulized-type noregu --EMtype celltypeEM  --clustering-method KMeans --imputeMode --npyDir npyImputeN2F_KMeans/
 
-python3 -W ignore main.py --datasetName 5.Goolam --n-clusters 5 --regulized-type noregu --EMtype celltypeEM  --clustering-method KMeans --imputeMode --npyDir npyImputeG2F_KMeans/
-python3 -W ignore main.py --datasetName 5.Goolam --n-clusters 5 --discreteTag --regulized-type noregu --EMtype celltypeEM  --clustering-method KMeans --imputeMode --npyDir npyImputeG2F_KMeans/
-python3 -W ignore main.py --datasetName 5.Goolam_LTMG --n-clusters 5 --regulized-type noregu --EMtype celltypeEM  --clustering-method KMeans --imputeMode --npyDir npyImputeG2F_KMeans/
+python3 -W ignore main.py --datasetName 5.Goolam --n-clusters 5 --regulized-type noregu --EMtype celltypeEM  --clustering-method KMeans --imputeMode --npyDir npyImputeN2F_KMeans/
+python3 -W ignore main.py --datasetName 5.Goolam --n-clusters 5 --discreteTag --regulized-type noregu --EMtype celltypeEM  --clustering-method KMeans --imputeMode --npyDir npyImputeN2F_KMeans/
+python3 -W ignore main.py --datasetName 5.Goolam_LTMG --n-clusters 5 --regulized-type noregu --EMtype celltypeEM  --clustering-method KMeans --imputeMode --npyDir npyImputeN2F_KMeans/
 
-python3 -W ignore main.py --datasetName 7.Deng --n-clusters 10 --regulized-type noregu --EMtype celltypeEM  --clustering-method KMeans --imputeMode --npyDir npyImputeG2F_KMeans/
-python3 -W ignore main.py --datasetName 7.Deng --n-clusters 10 --discreteTag --regulized-type noregu --EMtype celltypeEM  --clustering-method KMeans --imputeMode --npyDir npyImputeG2F_KMeans/
-python3 -W ignore main.py --datasetName 7.Deng_LTMG --n-clusters 10 --regulized-type noregu --EMtype celltypeEM  --clustering-method KMeans --imputeMode --npyDir npyImputeG2F_KMeans/
+python3 -W ignore main.py --datasetName 7.Deng --n-clusters 10 --regulized-type noregu --EMtype celltypeEM  --clustering-method KMeans --imputeMode --npyDir npyImputeN2F_KMeans/
+python3 -W ignore main.py --datasetName 7.Deng --n-clusters 10 --discreteTag --regulized-type noregu --EMtype celltypeEM  --clustering-method KMeans --imputeMode --npyDir npyImputeN2F_KMeans/
+python3 -W ignore main.py --datasetName 7.Deng_LTMG --n-clusters 10 --regulized-type noregu --EMtype celltypeEM  --clustering-method KMeans --imputeMode --npyDir npyImputeN2F_KMeans/
 
-python3 -W ignore main.py --datasetName 8.Pollen --n-clusters 11 --regulized-type noregu --EMtype celltypeEM  --clustering-method KMeans --imputeMode --npyDir npyImputeG2F_KMeans/
-python3 -W ignore main.py --datasetName 8.Pollen --n-clusters 11 --discreteTag --regulized-type noregu --EMtype celltypeEM  --clustering-method KMeans --imputeMode --npyDir npyImputeG2F_KMeans/
-python3 -W ignore main.py --datasetName 8.Pollen_LTMG --n-clusters 11 --regulized-type noregu --EMtype celltypeEM  --clustering-method KMeans --imputeMode --npyDir npyImputeG2F_KMeans/
+python3 -W ignore main.py --datasetName 8.Pollen --n-clusters 11 --regulized-type noregu --EMtype celltypeEM  --clustering-method KMeans --imputeMode --npyDir npyImputeN2F_KMeans/
+python3 -W ignore main.py --datasetName 8.Pollen --n-clusters 11 --discreteTag --regulized-type noregu --EMtype celltypeEM  --clustering-method KMeans --imputeMode --npyDir npyImputeN2F_KMeans/
+python3 -W ignore main.py --datasetName 8.Pollen_LTMG --n-clusters 11 --regulized-type noregu --EMtype celltypeEM  --clustering-method KMeans --imputeMode --npyDir npyImputeN2F_KMeans/
 
-python3 -W ignore main.py --datasetName 11.Kolodziejczyk --n-clusters 3 --regulized-type noregu --EMtype celltypeEM  --clustering-method KMeans --imputeMode --npyDir npyImputeG2F_KMeans/
-python3 -W ignore main.py --datasetName 11.Kolodziejczyk --n-clusters 3 --discreteTag --regulized-type noregu --EMtype celltypeEM  --clustering-method KMeans --imputeMode --npyDir npyImputeG2F_KMeans/
-python3 -W ignore main.py --datasetName 11.Kolodziejczyk_LTMG --n-clusters 3 --regulized-type noregu --EMtype celltypeEM  --clustering-method KMeans --imputeMode --npyDir npyImputeG2F_KMeans/
+python3 -W ignore main.py --datasetName 11.Kolodziejczyk --n-clusters 3 --regulized-type noregu --EMtype celltypeEM  --clustering-method KMeans --imputeMode --npyDir npyImputeN2F_KMeans/
+python3 -W ignore main.py --datasetName 11.Kolodziejczyk --n-clusters 3 --discreteTag --regulized-type noregu --EMtype celltypeEM  --clustering-method KMeans --imputeMode --npyDir npyImputeN2F_KMeans/
+python3 -W ignore main.py --datasetName 11.Kolodziejczyk_LTMG --n-clusters 3 --regulized-type noregu --EMtype celltypeEM  --clustering-method KMeans --imputeMode --npyDir npyImputeN2F_KMeans/
--- experiment_2_n_e_AffinityPropagation.sh
+++ experiment_2_n_e_AffinityPropagation.sh
@@ -1,34 +1,34 @@
-python3 -W ignore main.py --datasetName MMPbasal --regulized-type noregu --EMtype celltypeEM  --clustering-method AffinityPropagation --useGAEembedding --npyDir npyG2E_AffinityPropagation/
-python3 -W ignore main.py --datasetName MMPbasal --regulized-type noregu --discreteTag --EMtype celltypeEM  --clustering-method AffinityPropagation --useGAEembedding  --npyDir npyG2E_AffinityPropagation/
-python3 -W ignore main.py --datasetName MMPbasal_LTMG --regulized-type noregu --EMtype celltypeEM  --clustering-method AffinityPropagation --useGAEembedding  --npyDir npyG2E_AffinityPropagation/
+python3 -W ignore main.py --datasetName MMPbasal --regulized-type noregu --EMtype celltypeEM  --clustering-method AffinityPropagation --useGAEembedding --npyDir npyN2E_AffinityPropagation/
+python3 -W ignore main.py --datasetName MMPbasal --regulized-type noregu --discreteTag --EMtype celltypeEM  --clustering-method AffinityPropagation --useGAEembedding  --npyDir npyN2E_AffinityPropagation/
+python3 -W ignore main.py --datasetName MMPbasal_LTMG --regulized-type noregu --EMtype celltypeEM  --clustering-method AffinityPropagation --useGAEembedding  --npyDir npyN2E_AffinityPropagation/
 
-python3 -W ignore main.py --datasetName MMPbasal_all --regulized-type noregu --EMtype celltypeEM  --clustering-method AffinityPropagation --useGAEembedding --npyDir npyG2E_AffinityPropagation/
-python3 -W ignore main.py --datasetName MMPbasal_all --regulized-type noregu --discreteTag --EMtype celltypeEM  --clustering-method AffinityPropagation --useGAEembedding --npyDir npyG2E_AffinityPropagation/
-python3 -W ignore main.py --datasetName MMPbasal_all_LTMG --regulized-type noregu --EMtype celltypeEM  --clustering-method AffinityPropagation --useGAEembedding --npyDir npyG2E_AffinityPropagation/
+python3 -W ignore main.py --datasetName MMPbasal_all --regulized-type noregu --EMtype celltypeEM  --clustering-method AffinityPropagation --useGAEembedding --npyDir npyN2E_AffinityPropagation/
+python3 -W ignore main.py --datasetName MMPbasal_all --regulized-type noregu --discreteTag --EMtype celltypeEM  --clustering-method AffinityPropagation --useGAEembedding --npyDir npyN2E_AffinityPropagation/
+python3 -W ignore main.py --datasetName MMPbasal_all_LTMG --regulized-type noregu --EMtype celltypeEM  --clustering-method AffinityPropagation --useGAEembedding --npyDir npyN2E_AffinityPropagation/
 
-python3 -W ignore main.py --datasetName MMPbasal_allcell --regulized-type noregu --EMtype celltypeEM  --clustering-method AffinityPropagation --useGAEembedding --npyDir npyG2E_AffinityPropagation/
-python3 -W ignore main.py --datasetName MMPbasal_allcell --regulized-type noregu --discreteTag --EMtype celltypeEM  --clustering-method AffinityPropagation --useGAEembedding --npyDir npyG2E_AffinityPropagation/
+python3 -W ignore main.py --datasetName MMPbasal_allcell --regulized-type noregu --EMtype celltypeEM  --clustering-method AffinityPropagation --useGAEembedding --npyDir npyN2E_AffinityPropagation/
+python3 -W ignore main.py --datasetName MMPbasal_allcell --regulized-type noregu --discreteTag --EMtype celltypeEM  --clustering-method AffinityPropagation --useGAEembedding --npyDir npyN2E_AffinityPropagation/
 
-python3 -W ignore main.py --datasetName MMPbasal_2000 --regulized-type noregu --EMtype celltypeEM  --clustering-method AffinityPropagation --useGAEembedding --npyDir npyG2E_AffinityPropagation/
-python3 -W ignore main.py --datasetName MMPbasal_2000 --regulized-type noregu --discreteTag --EMtype celltypeEM  --clustering-method AffinityPropagation --useGAEembedding --npyDir npyG2E_AffinityPropagation/
-python3 -W ignore main.py --datasetName MMPbasal_2000_LTMG --regulized-type noregu --EMtype celltypeEM  --clustering-method AffinityPropagation --useGAEembedding --npyDir npyG2E_AffinityPropagation/
+python3 -W ignore main.py --datasetName MMPbasal_2000 --regulized-type noregu --EMtype celltypeEM  --clustering-method AffinityPropagation --useGAEembedding --npyDir npyN2E_AffinityPropagation/
+python3 -W ignore main.py --datasetName MMPbasal_2000 --regulized-type noregu --discreteTag --EMtype celltypeEM  --clustering-method AffinityPropagation --useGAEembedding --npyDir npyN2E_AffinityPropagation/
+python3 -W ignore main.py --datasetName MMPbasal_2000_LTMG --regulized-type noregu --EMtype celltypeEM  --clustering-method AffinityPropagation --useGAEembedding --npyDir npyN2E_AffinityPropagation/
 
-python3 -W ignore main.py --datasetName 4.Yan --n-clusters 7 --regulized-type noregu --EMtype celltypeEM  --clustering-method AffinityPropagation --useGAEembedding --npyDir npyG2E_AffinityPropagation/
-python3 -W ignore main.py --datasetName 4.Yan --n-clusters 7 --regulized-type noregu --discreteTag --EMtype celltypeEM  --clustering-method AffinityPropagation --useGAEembedding --npyDir npyG2E_AffinityPropagation/
-python3 -W ignore main.py --datasetName 4.Yan_LTMG --n-clusters 7 --regulized-type noregu --EMtype celltypeEM  --clustering-method AffinityPropagation --useGAEembedding --npyDir npyG2E_AffinityPropagation/
+python3 -W ignore main.py --datasetName 4.Yan --n-clusters 7 --regulized-type noregu --EMtype celltypeEM  --clustering-method AffinityPropagation --useGAEembedding --npyDir npyN2E_AffinityPropagation/
+python3 -W ignore main.py --datasetName 4.Yan --n-clusters 7 --regulized-type noregu --discreteTag --EMtype celltypeEM  --clustering-method AffinityPropagation --useGAEembedding --npyDir npyN2E_AffinityPropagation/
+python3 -W ignore main.py --datasetName 4.Yan_LTMG --n-clusters 7 --regulized-type noregu --EMtype celltypeEM  --clustering-method AffinityPropagation --useGAEembedding --npyDir npyN2E_AffinityPropagation/
 
-python3 -W ignore main.py --datasetName 5.Goolam --n-clusters 5 --regulized-type noregu --EMtype celltypeEM  --clustering-method AffinityPropagation --useGAEembedding --npyDir npyG2E_AffinityPropagation/
-python3 -W ignore main.py --datasetName 5.Goolam --n-clusters 5 --regulized-type noregu --discreteTag --EMtype celltypeEM  --clustering-method AffinityPropagation --useGAEembedding --npyDir npyG2E_AffinityPropagation/
-python3 -W ignore main.py --datasetName 5.Goolam_LTMG --n-clusters 5 --regulized-type noregu --EMtype celltypeEM  --clustering-method AffinityPropagation --useGAEembedding --npyDir npyG2E_AffinityPropagation/
+python3 -W ignore main.py --datasetName 5.Goolam --n-clusters 5 --regulized-type noregu --EMtype celltypeEM  --clustering-method AffinityPropagation --useGAEembedding --npyDir npyN2E_AffinityPropagation/
+python3 -W ignore main.py --datasetName 5.Goolam --n-clusters 5 --regulized-type noregu --discreteTag --EMtype celltypeEM  --clustering-method AffinityPropagation --useGAEembedding --npyDir npyN2E_AffinityPropagation/
+python3 -W ignore main.py --datasetName 5.Goolam_LTMG --n-clusters 5 --regulized-type noregu --EMtype celltypeEM  --clustering-method AffinityPropagation --useGAEembedding --npyDir npyN2E_AffinityPropagation/
 
-python3 -W ignore main.py --datasetName 7.Deng --n-clusters 10 --regulized-type noregu --EMtype celltypeEM  --clustering-method AffinityPropagation --useGAEembedding --npyDir npyG2E_AffinityPropagation/
-python3 -W ignore main.py --datasetName 7.Deng --n-clusters 10 --regulized-type noregu --discreteTag --EMtype celltypeEM  --clustering-method AffinityPropagation --useGAEembedding --npyDir npyG2E_AffinityPropagation/
-python3 -W ignore main.py --datasetName 7.Deng_LTMG --n-clusters 10 --regulized-type noregu --EMtype celltypeEM  --clustering-method AffinityPropagation --useGAEembedding --npyDir npyG2E_AffinityPropagation/
+python3 -W ignore main.py --datasetName 7.Deng --n-clusters 10 --regulized-type noregu --EMtype celltypeEM  --clustering-method AffinityPropagation --useGAEembedding --npyDir npyN2E_AffinityPropagation/
+python3 -W ignore main.py --datasetName 7.Deng --n-clusters 10 --regulized-type noregu --discreteTag --EMtype celltypeEM  --clustering-method AffinityPropagation --useGAEembedding --npyDir npyN2E_AffinityPropagation/
+python3 -W ignore main.py --datasetName 7.Deng_LTMG --n-clusters 10 --regulized-type noregu --EMtype celltypeEM  --clustering-method AffinityPropagation --useGAEembedding --npyDir npyN2E_AffinityPropagation/
 
-python3 -W ignore main.py --datasetName 8.Pollen --n-clusters 11 --regulized-type noregu --EMtype celltypeEM  --clustering-method AffinityPropagation --useGAEembedding --npyDir npyG2E_AffinityPropagation/
-python3 -W ignore main.py --datasetName 8.Pollen --n-clusters 11 --regulized-type noregu --discreteTag --EMtype celltypeEM  --clustering-method AffinityPropagation --useGAEembedding --npyDir npyG2E_AffinityPropagation/
-python3 -W ignore main.py --datasetName 8.Pollen_LTMG --n-clusters 11 --regulized-type noregu --EMtype celltypeEM  --clustering-method AffinityPropagation --useGAEembedding --npyDir npyG2E_AffinityPropagation/
+python3 -W ignore main.py --datasetName 8.Pollen --n-clusters 11 --regulized-type noregu --EMtype celltypeEM  --clustering-method AffinityPropagation --useGAEembedding --npyDir npyN2E_AffinityPropagation/
+python3 -W ignore main.py --datasetName 8.Pollen --n-clusters 11 --regulized-type noregu --discreteTag --EMtype celltypeEM  --clustering-method AffinityPropagation --useGAEembedding --npyDir npyN2E_AffinityPropagation/
+python3 -W ignore main.py --datasetName 8.Pollen_LTMG --n-clusters 11 --regulized-type noregu --EMtype celltypeEM  --clustering-method AffinityPropagation --useGAEembedding --npyDir npyN2E_AffinityPropagation/
 
-python3 -W ignore main.py --datasetName 11.Kolodziejczyk --n-clusters 3 --regulized-type noregu --EMtype celltypeEM  --clustering-method AffinityPropagation --useGAEembedding --npyDir npyG2E_AffinityPropagation/
-python3 -W ignore main.py --datasetName 11.Kolodziejczyk --n-clusters 3 --regulized-type noregu --discreteTag --EMtype celltypeEM  --clustering-method AffinityPropagation --useGAEembedding --npyDir npyG2E_AffinityPropagation/
-python3 -W ignore main.py --datasetName 11.Kolodziejczyk_LTMG --n-clusters 3 --regulized-type noregu --EMtype celltypeEM  --clustering-method AffinityPropagation --useGAEembedding --npyDir npyG2E_AffinityPropagation/
+python3 -W ignore main.py --datasetName 11.Kolodziejczyk --n-clusters 3 --regulized-type noregu --EMtype celltypeEM  --clustering-method AffinityPropagation --useGAEembedding --npyDir npyN2E_AffinityPropagation/
+python3 -W ignore main.py --datasetName 11.Kolodziejczyk --n-clusters 3 --regulized-type noregu --discreteTag --EMtype celltypeEM  --clustering-method AffinityPropagation --useGAEembedding --npyDir npyN2E_AffinityPropagation/
+python3 -W ignore main.py --datasetName 11.Kolodziejczyk_LTMG --n-clusters 3 --regulized-type noregu --EMtype celltypeEM  --clustering-method AffinityPropagation --useGAEembedding --npyDir npyN2E_AffinityPropagation/
