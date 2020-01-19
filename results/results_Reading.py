0525e29a0f86394a34ecad3791d134f156ba51e7
Wang <wangjue@umsystem.edu>
2020-01-18 22:40:27 -0600

    Add split mode for results summary cluster, for it exceeds 48 hours of running time

--- results/results_Reading.py
+++ results/results_Reading.py
@@ -6,6 +6,10 @@ parser.add_argument('--methodName', type=int, default=0,
 parser.add_argument('--imputeMode', default=False, action='store_true',
                     help='impute or not (default: False). Caution: usually change npuDir if set imputeMode as true')
 parser.add_argument('--runMode',action='store_true', default=False, help="Run or prepare cluster script")
+parser.add_argument('--splitMode', default=False, action='store_true',
+                    help='whether split, used for long queue')
+parser.add_argument('--batchStr', type=int, default=0, 
+                    help="method used: 0-1")
 args = parser.parse_args()
 
 # Note:
@@ -33,6 +37,31 @@ datasetList = [
     '11.Kolodziejczyk --discreteTag'
     ]
     #TODO: we wait for 11.Kolodziejczyk_LTMG
+if args.splitMode:
+    if args.batchStr == 0:
+        datasetList = [
+        'MMPbasal_2000',
+        'MMPbasal_2000 --discreteTag',
+        'MMPbasal_2000_LTMG',
+        '4.Yan',
+        '4.Yan --discreteTag',
+        '4.Yan_LTMG',
+        '5.Goolam',
+        '5.Goolam --discreteTag',
+        '5.Goolam_LTMG',
+        ]
+    else:
+        datasetList = [
+        '7.Deng',
+        '7.Deng --discreteTag',
+        '7.Deng_LTMG',
+        '8.Pollen',
+        '8.Pollen --discreteTag',
+        '8.Pollen_LTMG',
+        '11.Kolodziejczyk',
+        '11.Kolodziejczyk --discreteTag'
+        ]
+
 
 if args.imputeMode:
     pyStr = 'results_impute.py'
@@ -120,7 +149,7 @@ else:
     cellIndexDir = '/home/jwang/data/scData/'
     
 def getBenchmarkStr(count):
-    benchmarkStr = ''    
+    benchmarkStr = ''
     if int(count/3)==1:
         benchmarkStr = ' --benchmark '\
             '--labelFilename ' + labelFileDir + '4.Yan/Yan_cell_label.csv '\
@@ -132,25 +161,46 @@ def getBenchmarkStr(count):
             '--labelFilename ' + labelFileDir + '5.Goolam/Goolam_cell_label.csv '\
             '--cellFilename ' + cellFileDir + '5.Goolam.cellname.txt '\
             '--cellIndexname ' + cellIndexDir + '5.Goolam/ind.5.Goolam.cellindex.txt '\
-            '--n-clusters 5 '
-    elif int(count/3)==3:
-        benchmarkStr = ' --benchmark '\
-            '--labelFilename ' + labelFileDir + '7.Deng/Deng_cell_label.csv '\
-            '--cellFilename ' + cellFileDir + '7.Deng.cellname.txt '\
-            '--cellIndexname ' + cellIndexDir + '7.Deng/ind.7.Deng.cellindex.txt '\
-            '--n-clusters 10 '
-    elif int(count/3)==4:
-        benchmarkStr = ' --benchmark '\
-            '--labelFilename ' + labelFileDir + '8.Pollen/Pollen_cell_label.csv '\
-            '--cellFilename ' + cellFileDir + '8.Pollen.cellname.txt '\
-            '--cellIndexname ' + cellIndexDir + '8.Pollen/ind.8.Pollen.cellindex.txt '\
-            '--n-clusters 11 '
-    elif int(count/3)==5:
-        benchmarkStr = ' --benchmark '\
-            '--labelFilename ' + labelFileDir + '11.Kolodziejczyk/Kolodziejczyk_cell_label.csv '\
-            '--cellFilename ' + cellFileDir + '11.Kolodziejczyk.cellname.txt '\
-            '--cellIndexname ' + cellIndexDir + '11.Kolodziejczyk/ind.11.Kolodziejczyk.cellindex.txt '\
-            '--n-clusters 3 '
+            '--n-clusters 5 '    
+    if not args.splitMode: 
+        if int(count/3)==3:
+            benchmarkStr = ' --benchmark '\
+                '--labelFilename ' + labelFileDir + '7.Deng/Deng_cell_label.csv '\
+                '--cellFilename ' + cellFileDir + '7.Deng.cellname.txt '\
+                '--cellIndexname ' + cellIndexDir + '7.Deng/ind.7.Deng.cellindex.txt '\
+                '--n-clusters 10 '   
+        elif int(count/3)==4:
+            benchmarkStr = ' --benchmark '\
+                '--labelFilename ' + labelFileDir + '8.Pollen/Pollen_cell_label.csv '\
+                '--cellFilename ' + cellFileDir + '8.Pollen.cellname.txt '\
+                '--cellIndexname ' + cellIndexDir + '8.Pollen/ind.8.Pollen.cellindex.txt '\
+                '--n-clusters 11 '
+        elif int(count/3)==5:
+            benchmarkStr = ' --benchmark '\
+                '--labelFilename ' + labelFileDir + '11.Kolodziejczyk/Kolodziejczyk_cell_label.csv '\
+                '--cellFilename ' + cellFileDir + '11.Kolodziejczyk.cellname.txt '\
+                '--cellIndexname ' + cellIndexDir + '11.Kolodziejczyk/ind.11.Kolodziejczyk.cellindex.txt '\
+                '--n-clusters 3 '
+    else:
+        if not args.batchStr == 0:
+            if int(count/3)==0:
+                benchmarkStr = ' --benchmark '\
+                    '--labelFilename ' + labelFileDir + '7.Deng/Deng_cell_label.csv '\
+                    '--cellFilename ' + cellFileDir + '7.Deng.cellname.txt '\
+                    '--cellIndexname ' + cellIndexDir + '7.Deng/ind.7.Deng.cellindex.txt '\
+                    '--n-clusters 10 '
+            elif int(count/3)==1:
+                benchmarkStr = ' --benchmark '\
+                    '--labelFilename ' + labelFileDir + '8.Pollen/Pollen_cell_label.csv '\
+                    '--cellFilename ' + cellFileDir + '8.Pollen.cellname.txt '\
+                    '--cellIndexname ' + cellIndexDir + '8.Pollen/ind.8.Pollen.cellindex.txt '\
+                    '--n-clusters 11 '
+            elif int(count/3)==2:
+                benchmarkStr = ' --benchmark '\
+                    '--labelFilename ' + labelFileDir + '11.Kolodziejczyk/Kolodziejczyk_cell_label.csv '\
+                    '--cellFilename ' + cellFileDir + '11.Kolodziejczyk.cellname.txt '\
+                    '--cellIndexname ' + cellIndexDir + '11.Kolodziejczyk/ind.11.Kolodziejczyk.cellindex.txt '\
+                    '--n-clusters 3 '
     return benchmarkStr
 
 
@@ -159,11 +209,14 @@ if not args.runMode:
         imputeStr = 'I'
     else:
         imputeStr = 'C'
+    splitStr = ''
+    if args.splitMode:
+        splitStr = '_'+str(args.batchStr)+'_'
     templateStr = "#! /bin/bash\n"\
     "######################### Batch Headers #########################\n"\
     "#SBATCH -A xulab\n"\
     "#SBATCH -p BioCompute               # use the BioCompute partition\n"\
-    "#SBATCH -J R" + imputeStr + str(args.methodName) +              " \n"\
+    "#SBATCH -J R" + imputeStr + splitStr + str(args.methodName) +              " \n"\
     "#SBATCH -o results-%j.out           # give the job output a custom name\n"\
     "#SBATCH -t 2-00:00                  # two days time limit\n"\
     "#SBATCH -N 1                        # number of nodes\n"\
--- results/submitCluster_Result_Celltype.sh
+++ results/submitCluster_Result_Celltype.sh
@@ -1,11 +1,30 @@
+# for i in {0..27}
+# do
+# python results_Reading.py --methodName $i > run_Results_Celltype_$i.sh
+# done
+
+# submit
+# for i in {0..27}
+# do
+# sbatch run_Results_Celltype_$i.sh
+# sleep 3
+# done
+
+#split mode
 for i in {0..27}
 do
-python results_Reading.py --methodName $i > run_Results_Celltype_$i.sh
+for j in {0..1}
+do
+python results_Reading.py --methodName $i --splitMode --batchStr $j > run_Results_Celltype_$i_$j.sh
+done
 done
 
 # submit
 for i in {0..27}
 do
-sbatch run_Results_Celltype_$i.sh
+for j in {0..1}
+do
+sbatch run_Results_Celltype_$i_$j.sh
 sleep 3
+done
 done
