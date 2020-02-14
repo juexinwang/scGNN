f57cf5ff26d7ebaf7646d54c7cce2a0fabc9f8d6
Wang <wangjue@umsystem.edu>
2020-02-14 11:57:30 -0600

    change iteration time

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ parser.add_argument('--datasetName', type=str, default='13.Zeisel',
 # Dataset: 1-13 benchmark: 1.Biase/2.Li/3.Treutlein/4.Yan/5.Goolam/6.Guo/7.Deng/8.Pollen/9.Chung/10.Usoskin/11.Kolodziejczyk/12.Klein/13.Zeisel
 parser.add_argument('--batch-size', type=int, default=12800, metavar='N',
                     help='input batch size for training (default: 128)')
-parser.add_argument('--epochs', type=int, default=200, metavar='N',
+parser.add_argument('--epochs', type=int, default=500, metavar='N',
                     help='number of epochs to train (default: 500)')
 parser.add_argument('--EM-iteration', type=int, default=5, metavar='N',
                     help='number of epochs in EM iteration (default: 3)')
