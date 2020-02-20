8d247038b5aa0a5f8fbb3d050201a5e4a917fbc4
Wang <wangjue@umsystem.edu>
2020-02-20 10:41:54 -0600

    split mode for imputation results

--- results/submitCluster_Result_Impute.sh
+++ results/submitCluster_Result_Impute.sh
@@ -1,11 +1,28 @@
-for i in {0..62}
-do
-python results_Reading.py --methodName $i --imputeMode > run_Results_Impute_$i.sh
-done
+# for i in {0..62}
+# do
+# python results_Reading.py --methodName $i --imputeMode > run_Results_Impute_$i.sh
+# done
 
 # # submit
 # for i in {0..62}
 # do
 # sbatch run_Results_Impute_$i.sh
 # sleep 1
+# done
+
+## split mode
+for i in {0..62}
+do
+for j in {0..14}
+python results_Reading.py --methodName $i --splitMode --batchStr $j --imputeMode > run_Results_Impute_$i-$j.sh
+done
+done
+
+# submit
+# for i in {0..62}
+# do
+# for j in {0..14}
+# sbatch run_Results_Impute_$i-$j.sh
+# sleep 1
+# done
 # done
