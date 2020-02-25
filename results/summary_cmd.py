2150a8c1b58c0204b9ffbcc1f78c330e48a6b154
Wang <wangjue@umsystem.edu>
2020-02-25 15:31:01 -0600

    check summary results

--- results/summary_cmd.py
+++ results/summary_cmd.py
@@ -26,28 +26,29 @@ for i in range(63):
         # print(commandStr)
         os.system(commandStr)
 
-numDict={0:'z',1:'z0',2:'z1',3:'z2',4:'z3',5:'z4',6:'z5',7:'z6',8:'z7',9:'z8',10:'z9',11:'z',12:'z0',13:'z1',14:'z2',15:'z3',16:'z4',17:'z5',18:'z6',19:'z7',20:'z8',21:'z9'}
+if args.imputeMode:
+    numDict={0:'z',1:'z0',2:'z1',3:'z2',4:'z3',5:'z4',6:'z5',7:'z6',8:'z7',9:'z8',10:'z9',11:'z',12:'z0',13:'z1',14:'z2',15:'z3',16:'z4',17:'z5',18:'z6',19:'z7',20:'z8',21:'z9'}
 
-for i in range(13):
-    allstr = []
-    for j in range(63):
-        tag = True
-        with open('imputation/RI_'+str(j)+'_'+str(i)+'.txt') as f:
-            lines = f.readlines()
-            count = 0
-            for line in lines:
-                if line.startswith('Traceback (most recent call last):'):
-                    tag = False
-                elif line.startswith('FileNotFoundError:'):
-                    tag = True
-                    count += 1
-                elif tag:
-                    allstr.append(str(j)+'\t'+numDict[count]+'\t'+line)
-                    count += 1
-            f.close()
-    
-    with open('imputation/results_'+str(i)+'.txt','w') as fw:
-        fw.writelines("%s" % strr for strr in allstr)
-        fw.close()
+    for i in range(13):
+        allstr = []
+        for j in range(63):
+            tag = True
+            with open('imputation/RI_'+str(j)+'_'+str(i)+'.txt') as f:
+                lines = f.readlines()
+                count = 0
+                for line in lines:
+                    if line.startswith('Traceback (most recent call last):'):
+                        tag = False
+                    elif line.startswith('FileNotFoundError:'):
+                        tag = True
+                        count += 1
+                    elif tag:
+                        allstr.append(str(j)+'\t'+numDict[count]+'\t'+line)
+                        count += 1
+                f.close()
+        
+        with open('imputation/results_'+str(i)+'.txt','w') as fw:
+            fw.writelines("%s" % strr for strr in allstr)
+            fw.close()
     
 
