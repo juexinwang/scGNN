fb587fc470a4f99e91ff728955229ef824d9543f
Wang <wangjue@umsystem.edu>
2020-02-25 10:22:56 -0600

    change results

--- results/summary_cmd.py
+++ results/summary_cmd.py
@@ -3,7 +3,6 @@ import argparse
 parser = argparse.ArgumentParser(description='Read Results from results bach scripts')
 parser.add_argument('--imputeMode', default=False, action='store_true',
                     help='impute or not (default: False). Caution: usually change npuDir if set imputeMode as true')
-parser.add_argument('--runMode',action='store_true', default=False, help="Run or prepare cluster script")
 args = parser.parse_args()
 
 reDict = {}
@@ -18,10 +17,12 @@ with open('jobinfo_usage.txt') as f:
 for i in range(63):
     for j in range(13):
         # 'python summary.py --fileName results-19687313.out --outFileName RC_0_0.txt'
-        # name = 'RC_'+str(i)+'_'+str(j)
-        name = 'RI_'+str(i)+'_'+str(j)
-        commandStr = 'python summary.py --fileName results-' + reDict[name] + '.out --outFileName ' + name + '.txt'
-        print(commandStr)
-        # os.system(commandStr)
+        name = 'RC_'+str(i)+'_'+str(j)
+        if args.imputeMode:
+            commandStr = 'python summary.py --fileName results-' + reDict[name] + '.out --outFileName celltype/' + name + '.txt'    
+        else:
+            commandStr = 'cat results-' + reDict[name] + '.out > imputation/' + name + '.txt'
+        # print(commandStr)
+        os.system(commandStr)
     
 
