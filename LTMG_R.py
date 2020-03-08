ee7de6ececa7902a70c2a5429480f7f028f5e7af
Wang <wangjue@umsystem.edu>
2020-03-08 14:30:56 -0500

    Fix a minor bug of R LTMG

--- LTMG_R.py
+++ LTMG_R.py
@@ -6,8 +6,8 @@ def runLTMG(expressionFile,ltmgFile):
     # robjects.r('''
     #         setwd("/users/PAS1475/qiren081/GCNN/data/sc/ex")
     #         test.data <- read.csv("Biase_expression.csv",header = T,row.names = 1,check.names = F)
-    #         object <- scGNN.LTMG::CreateLTMGObject(as.matrix(test.data))
-    #         object <- scGNN.LTMG::RunLTMG(object,Gene_use = "all",seed =123,k=5)
+    #         object <- scGNNLTMG::CreateLTMGObject(as.matrix(test.data))
+    #         object <- scGNNLTMG::RunLTMG(object,Gene_use = "all",seed =123,k=5)
     #         my.matrix <- cbind(ID = rownames(object@OrdinalMatrix),object@OrdinalMatrix)
     #         write.table(my.matrix, file = "LTMG_discretization_Bia.txt",row.names = F, quote = F,sep = "\t")
     # ''')
@@ -15,8 +15,8 @@ def runLTMG(expressionFile,ltmgFile):
     robjects.globalenv['ltmgFile'] = ltmgFile
     robjects.r('''
             test.data <- read.csv(expressionFile,header = T,row.names = 1,check.names = F)
-            object <- scGNN.LTMG::CreateLTMGObject(as.matrix(test.data))
-            object <- scGNN.LTMG::RunLTMG(object,Gene_use = "all",seed =123,k=5)
+            object <- scGNNLTMG::CreateLTMGObject(as.matrix(test.data))
+            object <- scGNNLTMG::RunLTMG(object,Gene_use = "all",seed =123,k=5)
             my.matrix <- cbind(ID = rownames(object@OrdinalMatrix),object@OrdinalMatrix)
             write.table(my.matrix, file = ltmgFile,row.names = F, quote = F,sep = "\t")
     ''')
