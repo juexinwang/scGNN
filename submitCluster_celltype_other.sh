7b4fb9f5473c0f086430559f6f7ec65e6b4422dc
juexinwang <juexinwang@hotmail.com>
2020-03-13 21:24:27 -0500

    Add filenames

---
+++ submitCluster_celltype_other.sh
@@ -0,0 +1,177 @@
+mkdir npyG2B_AgglomerativeClusteringW
+mkdir npyG2E_AgglomerativeClusteringW
+mkdir npyG2F_AgglomerativeClusteringW
+mkdir npyR2B_AgglomerativeClusteringW
+mkdir npyR2E_AgglomerativeClusteringW
+mkdir npyR2F_AgglomerativeClusteringW
+mkdir npyN2B_AgglomerativeClusteringW
+mkdir npyN2E_AgglomerativeClusteringW
+mkdir npyN2F_AgglomerativeClusteringW
+
+mkdir npyG2B_AgglomerativeClusteringA
+mkdir npyG2E_AgglomerativeClusteringA
+mkdir npyG2F_AgglomerativeClusteringA
+mkdir npyR2B_AgglomerativeClusteringA
+mkdir npyR2E_AgglomerativeClusteringA
+mkdir npyR2F_AgglomerativeClusteringA
+mkdir npyN2B_AgglomerativeClusteringA
+mkdir npyN2E_AgglomerativeClusteringA
+mkdir npyN2F_AgglomerativeClusteringA
+
+mkdir npyG2B_AgglomerativeClusteringC
+mkdir npyG2E_AgglomerativeClusteringC
+mkdir npyG2F_AgglomerativeClusteringC
+mkdir npyR2B_AgglomerativeClusteringC
+mkdir npyR2E_AgglomerativeClusteringC
+mkdir npyR2F_AgglomerativeClusteringC
+mkdir npyN2B_AgglomerativeClusteringC
+mkdir npyN2E_AgglomerativeClusteringC
+mkdir npyN2F_AgglomerativeClusteringC
+
+mkdir npyG2B_AgglomerativeClusteringS
+mkdir npyG2E_AgglomerativeClusteringS
+mkdir npyG2F_AgglomerativeClusteringS
+mkdir npyR2B_AgglomerativeClusteringS
+mkdir npyR2E_AgglomerativeClusteringS
+mkdir npyR2F_AgglomerativeClusteringS
+mkdir npyN2B_AgglomerativeClusteringS
+mkdir npyN2E_AgglomerativeClusteringS
+mkdir npyN2F_AgglomerativeClusteringS
+
+mkdir npyG2B_MeanShift
+mkdir npyG2E_MeanShift
+mkdir npyG2F_MeanShift
+mkdir npyR2B_MeanShift
+mkdir npyR2E_MeanShift
+mkdir npyR2F_MeanShift
+mkdir npyN2B_MeanShift
+mkdir npyN2E_MeanShift
+mkdir npyN2F_MeanShift
+
+mkdir npyG2B_OPTICS
+mkdir npyG2E_OPTICS
+mkdir npyG2F_OPTICS
+mkdir npyR2B_OPTICS
+mkdir npyR2E_OPTICS
+mkdir npyR2F_OPTICS
+mkdir npyN2B_OPTICS
+mkdir npyN2E_OPTICS
+mkdir npyN2F_OPTICS
+
+for i in {1..13}
+do
+# sbatch run_experiment_2_g_b_AgglomerativeClusteringW_$i.sh
+# sleep 1
+# sbatch run_experiment_2_g_e_AgglomerativeClusteringW_$i.sh
+# sleep 1
+# sbatch run_experiment_2_g_f_AgglomerativeClusteringW_$i.sh
+# sleep 1
+# sbatch run_experiment_2_r_b_AgglomerativeClusteringW_$i.sh
+# sleep 1
+# sbatch run_experiment_2_r_e_AgglomerativeClusteringW_$i.sh
+# sleep 1
+# sbatch run_experiment_2_r_f_AgglomerativeClusteringW_$i.sh
+# sleep 1
+# sbatch run_experiment_2_n_b_AgglomerativeClusteringW_$i.sh
+# sleep 1
+# sbatch run_experiment_2_n_e_AgglomerativeClusteringW_$i.sh
+# sleep 1
+# sbatch run_experiment_2_n_f_AgglomerativeClusteringW_$i.sh
+# sleep 1
+
+# sbatch run_experiment_2_g_b_AgglomerativeClusteringA_$i.sh
+# sleep 1
+# sbatch run_experiment_2_g_e_AgglomerativeClusteringA_$i.sh
+# sleep 1
+# sbatch run_experiment_2_g_f_AgglomerativeClusteringA_$i.sh
+# sleep 1
+# sbatch run_experiment_2_r_b_AgglomerativeClusteringA_$i.sh
+# sleep 1
+# sbatch run_experiment_2_r_e_AgglomerativeClusteringA_$i.sh
+# sleep 1
+# sbatch run_experiment_2_r_f_AgglomerativeClusteringA_$i.sh
+# sleep 1
+# sbatch run_experiment_2_n_b_AgglomerativeClusteringA_$i.sh
+# sleep 1
+# sbatch run_experiment_2_n_e_AgglomerativeClusteringA_$i.sh
+# sleep 1
+# sbatch run_experiment_2_n_f_AgglomerativeClusteringA_$i.sh
+# sleep 1
+
+# sbatch run_experiment_2_g_b_AgglomerativeClusteringC_$i.sh
+# sleep 1
+# sbatch run_experiment_2_g_e_AgglomerativeClusteringC_$i.sh
+# sleep 1
+# sbatch run_experiment_2_g_f_AgglomerativeClusteringC_$i.sh
+# sleep 1
+# sbatch run_experiment_2_r_b_AgglomerativeClusteringC_$i.sh
+# sleep 1
+# sbatch run_experiment_2_r_e_AgglomerativeClusteringC_$i.sh
+# sleep 1
+# sbatch run_experiment_2_r_f_AgglomerativeClusteringC_$i.sh
+# sleep 1
+# sbatch run_experiment_2_n_b_AgglomerativeClusteringC_$i.sh
+# sleep 1
+# sbatch run_experiment_2_n_e_AgglomerativeClusteringC_$i.sh
+# sleep 1
+# sbatch run_experiment_2_n_f_AgglomerativeClusteringC_$i.sh
+# sleep 1
+
+# sbatch run_experiment_2_g_b_AgglomerativeClusteringS_$i.sh
+# sleep 1
+# sbatch run_experiment_2_g_e_AgglomerativeClusteringS_$i.sh
+# sleep 1
+# sbatch run_experiment_2_g_f_AgglomerativeClusteringS_$i.sh
+# sleep 1
+# sbatch run_experiment_2_r_b_AgglomerativeClusteringS_$i.sh
+# sleep 1
+# sbatch run_experiment_2_r_e_AgglomerativeClusteringS_$i.sh
+# sleep 1
+# sbatch run_experiment_2_r_f_AgglomerativeClusteringS_$i.sh
+# sleep 1
+# sbatch run_experiment_2_n_b_AgglomerativeClusteringS_$i.sh
+# sleep 1
+# sbatch run_experiment_2_n_e_AgglomerativeClusteringS_$i.sh
+# sleep 1
+# sbatch run_experiment_2_n_f_AgglomerativeClusteringS_$i.sh
+# sleep 1
+
+
+sbatch run_experiment_2_g_b_MeanShift_$i.sh
+sleep 1
+sbatch run_experiment_2_g_e_MeanShift_$i.sh
+sleep 1
+sbatch run_experiment_2_g_f_MeanShift_$i.sh
+sleep 1
+sbatch run_experiment_2_r_b_MeanShift_$i.sh
+sleep 1
+sbatch run_experiment_2_r_e_MeanShift_$i.sh
+sleep 1
+sbatch run_experiment_2_r_f_MeanShift_$i.sh
+sleep 1
+sbatch run_experiment_2_n_b_MeanShift_$i.sh
+sleep 1
+sbatch run_experiment_2_n_e_MeanShift_$i.sh
+sleep 1
+sbatch run_experiment_2_n_f_MeanShift_$i.sh
+sleep 1
+
+sbatch run_experiment_2_g_b_OPTICS_$i.sh
+sleep 1
+sbatch run_experiment_2_g_e_OPTICS_$i.sh
+sleep 1
+sbatch run_experiment_2_g_f_OPTICS_$i.sh
+sleep 1
+sbatch run_experiment_2_r_b_OPTICS_$i.sh
+sleep 1
+sbatch run_experiment_2_r_e_OPTICS_$i.sh
+sleep 1
+sbatch run_experiment_2_r_f_OPTICS_$i.sh
+sleep 1
+sbatch run_experiment_2_n_b_OPTICS_$i.sh
+sleep 1
+sbatch run_experiment_2_n_e_OPTICS_$i.sh
+sleep 1
+sbatch run_experiment_2_n_f_OPTICS_$i.sh
+sleep 1
+done
