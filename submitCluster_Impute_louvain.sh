107421e2a662506c84eafe69fb23721a9747fa6c
Wang <wangjue@umsystem.edu>
2020-03-15 11:45:48 -0500

    update submit

--- submitCluster_Impute_louvain.sh
+++ submitCluster_Impute_louvain.sh
@@ -57,79 +57,79 @@ mkdir npyImputeN2F_LB
 
 for i in {1..13}
 do
-sbatch run_experimentImpute_1_g_b_$i.sh
-sleep 1
-sbatch run_experimentImpute_1_g_e_$i.sh
-sleep 1
-sbatch run_experimentImpute_1_g_f_$i.sh
-sleep 1
-sbatch run_experimentImpute_1_r_b_$i.sh
-sleep 1
-sbatch run_experimentImpute_1_r_e_$i.sh
-sleep 1
-sbatch run_experimentImpute_1_r_f_$i.sh
-sleep 1
-sbatch run_experimentImpute_1_n_b_$i.sh
-sleep 1
-sbatch run_experimentImpute_1_n_e_$i.sh
-sleep 1
-sbatch run_experimentImpute_1_n_f_$i.sh
-sleep 1
-sbatch run_experimentImpute_2_g_b_$i.sh
-sleep 1
-sbatch run_experimentImpute_2_g_e_$i.sh
-sleep 1
-sbatch run_experimentImpute_2_g_f_$i.sh
-sleep 1
-sbatch run_experimentImpute_2_r_b_$i.sh
-sleep 1
-sbatch run_experimentImpute_2_r_e_$i.sh
-sleep 1
-sbatch run_experimentImpute_2_r_f_$i.sh
-sleep 1
-sbatch run_experimentImpute_2_n_b_$i.sh
-sleep 1
-sbatch run_experimentImpute_2_n_e_$i.sh
-sleep 1
-sbatch run_experimentImpute_2_n_f_$i.sh
-sleep 1
+# sbatch run_experimentImpute_1_g_b_$i.sh
+# sleep 1
+# sbatch run_experimentImpute_1_g_e_$i.sh
+# sleep 1
+# sbatch run_experimentImpute_1_g_f_$i.sh
+# sleep 1
+# sbatch run_experimentImpute_1_r_b_$i.sh
+# sleep 1
+# sbatch run_experimentImpute_1_r_e_$i.sh
+# sleep 1
+# sbatch run_experimentImpute_1_r_f_$i.sh
+# sleep 1
+# sbatch run_experimentImpute_1_n_b_$i.sh
+# sleep 1
+# sbatch run_experimentImpute_1_n_e_$i.sh
+# sleep 1
+# sbatch run_experimentImpute_1_n_f_$i.sh
+# sleep 1
+# sbatch run_experimentImpute_2_g_b_$i.sh
+# sleep 1
+# sbatch run_experimentImpute_2_g_e_$i.sh
+# sleep 1
+# sbatch run_experimentImpute_2_g_f_$i.sh
+# sleep 1
+# sbatch run_experimentImpute_2_r_b_$i.sh
+# sleep 1
+# sbatch run_experimentImpute_2_r_e_$i.sh
+# sleep 1
+# sbatch run_experimentImpute_2_r_f_$i.sh
+# sleep 1
+# sbatch run_experimentImpute_2_n_b_$i.sh
+# sleep 1
+# sbatch run_experimentImpute_2_n_e_$i.sh
+# sleep 1
+# sbatch run_experimentImpute_2_n_f_$i.sh
+# sleep 1
 
-sbatch run_experimentImpute_1_g_b_LK_$i.sh
-sleep 1
-sbatch run_experimentImpute_1_g_e_LK_$i.sh
-sleep 1
-sbatch run_experimentImpute_1_g_f_LK_$i.sh
-sleep 1
-sbatch run_experimentImpute_1_r_b_LK_$i.sh
-sleep 1
-sbatch run_experimentImpute_1_r_e_LK_$i.sh
-sleep 1
-sbatch run_experimentImpute_1_r_f_LK_$i.sh
-sleep 1
-sbatch run_experimentImpute_1_n_b_LK_$i.sh
-sleep 1
-sbatch run_experimentImpute_1_n_e_LK_$i.sh
-sleep 1
-sbatch run_experimentImpute_1_n_f_LK_$i.sh
-sleep 1
-sbatch run_experimentImpute_2_g_b_LK_$i.sh
-sleep 1
-sbatch run_experimentImpute_2_g_e_LK_$i.sh
-sleep 1
-sbatch run_experimentImpute_2_g_f_LK_$i.sh
-sleep 1
-sbatch run_experimentImpute_2_r_b_LK_$i.sh
-sleep 1
-sbatch run_experimentImpute_2_r_e_LK_$i.sh
-sleep 1
-sbatch run_experimentImpute_2_r_f_LK_$i.sh
-sleep 1
-sbatch run_experimentImpute_2_n_b_LK_$i.sh
-sleep 1
-sbatch run_experimentImpute_2_n_e_LK_$i.sh
-sleep 1
-sbatch run_experimentImpute_2_n_f_LK_$i.sh
-sleep 1
+# sbatch run_experimentImpute_1_g_b_LK_$i.sh
+# sleep 1
+# sbatch run_experimentImpute_1_g_e_LK_$i.sh
+# sleep 1
+# sbatch run_experimentImpute_1_g_f_LK_$i.sh
+# sleep 1
+# sbatch run_experimentImpute_1_r_b_LK_$i.sh
+# sleep 1
+# sbatch run_experimentImpute_1_r_e_LK_$i.sh
+# sleep 1
+# sbatch run_experimentImpute_1_r_f_LK_$i.sh
+# sleep 1
+# sbatch run_experimentImpute_1_n_b_LK_$i.sh
+# sleep 1
+# sbatch run_experimentImpute_1_n_e_LK_$i.sh
+# sleep 1
+# sbatch run_experimentImpute_1_n_f_LK_$i.sh
+# sleep 1
+# sbatch run_experimentImpute_2_g_b_LK_$i.sh
+# sleep 1
+# sbatch run_experimentImpute_2_g_e_LK_$i.sh
+# sleep 1
+# sbatch run_experimentImpute_2_g_f_LK_$i.sh
+# sleep 1
+# sbatch run_experimentImpute_2_r_b_LK_$i.sh
+# sleep 1
+# sbatch run_experimentImpute_2_r_e_LK_$i.sh
+# sleep 1
+# sbatch run_experimentImpute_2_r_f_LK_$i.sh
+# sleep 1
+# sbatch run_experimentImpute_2_n_b_LK_$i.sh
+# sleep 1
+# sbatch run_experimentImpute_2_n_e_LK_$i.sh
+# sleep 1
+# sbatch run_experimentImpute_2_n_f_LK_$i.sh
+# sleep 1
 
 sbatch run_experimentImpute_1_g_b_LB_$i.sh
 sleep 1
