4e5f7f092892d320aeff3c9a6df633350b131355
Wang <wangjue@umsystem.edu>
2020-03-16 08:10:24 -0500

    results update submit

--- results/summary_cmd.py
+++ results/summary_cmd.py
@@ -20,9 +20,9 @@ with open(filename) as f:
         reDict[words[2]] = words[0]
     f.close()
 
-# complex: 63, select: 20, strong: 16
-for i in range(16):
-    for j in range(13):
+# complex: 63, select: 20, strong: 16, louvain: 54
+for i in range(54):
+    for j in range(12):
         # 'python summary.py --fileName results-19687313.out --outFileName RC_0_0.txt'        
         if args.imputeMode:
             name = 'RI_'+str(i)+'_'+str(j)
@@ -37,7 +37,7 @@ numDict={0:'z',1:'z0',2:'z1',3:'z2',4:'z3',5:'z4',6:'z5',7:'z6',8:'z7',9:'z8',10
 nameDict={'Z0':'','Z1':'','Z2':'','Z3':'','Z4':'','Z5':'','Z6':'','Z7':'','Z8':'','Z9':''}
 
 # for all
-for i in range(13):
+for i in range(12):
     allstr = []
     selstr = []
     # complex: 63, select: 20, strong: 16, louvain: 54 
