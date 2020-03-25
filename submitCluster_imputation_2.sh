8cacbd1deb18393b076a9e66173b0c3a8fccfdcb
juexinwang <juexinwang@hotmail.com>
2020-03-25 09:51:37 -0500

    update for std analysis

---
+++ submitCluster_imputation_2.sh
@@ -0,0 +1,16 @@
+mkdir npyImputeG2E_LK_2
+mkdir npyImputeR2E_LK_2
+mkdir npyImputeG2E_LB_2
+mkdir npyImputeR2E_LB_2
+
+for i in {1..13}
+do
+sbatch run_experimentImpute_2_g_e_LK_$i.sh
+sleep 1
+sbatch run_experimentImpute_2_r_e_LK_$i.sh
+sleep 1
+sbatch run_experimentImpute_2_g_e_LB_$i.sh
+sleep 1
+sbatch run_experimentImpute_2_r_e_LB_$i.sh
+sleep 1
+done
