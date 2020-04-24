695fac7df42a842ea87b2be0ad4f7e4940dce3e8
juexinwang <juexinwang@hotmail.com>
2020-04-23 23:22:45 -0500

    change to sparse mode

--- generatingCasestudy.py
+++ generatingCasestudy.py
@@ -34,7 +34,7 @@ datasetNameList = [
     'AD_GSE103334_NORMED_8CT', #2
     'E-GEOD-139324', #3 *
     'liver_cancer_GSE98638_11CT --nonsparseMode', #4 *
-    'liver_cancer_10X_20CT --nonsparseMode',  #5 *
+    'liver_cancer_10X_20CT',  #5 *
     'liver_cancer_smart_22CT --nonsparseMode', #6 *
     'GSM2388072', #7
     'BR', #8
