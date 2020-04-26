ecd968a0131f0d64cfffb84524927ca3c39a11bb
juexinwang <juexinwang@hotmail.com>
2020-04-26 00:08:12 -0500

    add convert

--- scripts/covert.sh
+++ scripts/covert.sh
@@ -8,10 +8,10 @@ python -W ignore covert.py --input '/storage/htc/joshilab/wangjue/scGNN/npyImput
 python -W ignore covert.py --input '/storage/htc/joshilab/wangjue/scGNN/npyImputeG2F/12.Klein_LTMG_0.1_features.npy' --output '12.G2F.original.csv'
 python -W ignore covert.py --input '/storage/htc/joshilab/wangjue/scGNN/npyImputeG2F/13.Zeisel_LTMG_0.1_features.npy' --output '13.G2F.original.csv'
 
-python -W ignore covert.py --input '/storage/htc/joshilab/wangjue/scGNN/npyImputeN2E/9.Chung_LTMG_0.1_features.npy' --output '9.N2E.original.csv'
-python -W ignore covert.py --input '/storage/htc/joshilab/wangjue/scGNN/npyImputeN2E/11.Kolodziejczyk_LTMG_0.1_features.npy' --output '11.N2E.original.csv'
-python -W ignore covert.py --input '/storage/htc/joshilab/wangjue/scGNN/npyImputeN2E/12.Klein_LTMG_0.1_features.npy' --output '12.N2E.original.csv'
-python -W ignore covert.py --input '/storage/htc/joshilab/wangjue/scGNN/npyImputeN2E/13.Zeisel_LTMG_0.1_features.npy' --output '13.N2E.original.csv'
+python -W ignore covert.py --input '/storage/htc/joshilab/wangjue/scGNN/npyImputeN2E/9.Chung_noregu_0.1_features.npy' --output '9.N2E.original.csv'
+python -W ignore covert.py --input '/storage/htc/joshilab/wangjue/scGNN/npyImputeN2E/11.Kolodziejczyk_noregu_0.1_features.npy' --output '11.N2E.original.csv'
+python -W ignore covert.py --input '/storage/htc/joshilab/wangjue/scGNN/npyImputeN2E/12.Klein_noregu_0.1_features.npy' --output '12.N2E.original.csv'
+python -W ignore covert.py --input '/storage/htc/joshilab/wangjue/scGNN/npyImputeN2E/13.Zeisel_noregu_0.1_features.npy' --output '13.N2E.original.csv'
 
 python -W ignore covert.py --input '/storage/htc/joshilab/wangjue/scGNN/npyImputeG1E/9.Chung_LTMG_0.1_features.npy' --output '9.G1E.original.csv'
 python -W ignore covert.py --input '/storage/htc/joshilab/wangjue/scGNN/npyImputeG1E/11.Kolodziejczyk_LTMG_0.1_features.npy' --output '11.G1E.original.csv'
