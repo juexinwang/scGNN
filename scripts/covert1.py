ec2b1f13cd376ac902ef8348c58aeffcff74e189
juexinwang <juexinwang@hotmail.com>
2020-04-26 00:40:53 -0500

    add convert

---
+++ scripts/covert1.py
@@ -0,0 +1,14 @@
+import numpy as np
+import argparse
+
+parser = argparse.ArgumentParser(description='convert npy to csv')
+parser.add_argument('--input', type=str, default='13.Zeisel_LTMG_0.1_features.npy',
+                    help='input')
+parser.add_argument('--output', type=str, default='13.csv',
+                    help='output')
+
+args = parser.parse_args()
+
+t=np.load(args.input,allow_pickle=True)
+np.savetxt(args.output,t1, delimiter=',',fmt='%d')
+
