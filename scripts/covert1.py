a9b6e61f2d41a73b4f96ea3b8527a1c5f2bcbf62
juexinwang <juexinwang@hotmail.com>
2020-04-26 00:43:36 -0500

    add convert

--- scripts/covert1.py
+++ scripts/covert1.py
@@ -10,5 +10,5 @@ parser.add_argument('--output', type=str, default='13.csv',
 args = parser.parse_args()
 
 t=np.load(args.input,allow_pickle=True)
-np.savetxt(args.output,t1, delimiter=',',fmt='%d')
+np.savetxt(args.output,t, delimiter=',',fmt='%d')
 
