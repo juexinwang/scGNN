6cbe34097ea2c0e7ff3758b5a63b1c977181e8aa
juexinwang <juexinwang@hotmail.com>
2020-05-02 09:00:42 -0500

    original imputation

--- submitCluster_imputation_2.sh
+++ submitCluster_imputation_2.sh
@@ -1,16 +1,16 @@
 mkdir npyImputeG2E_LK_2
-mkdir npyImputeR2E_LK_2
-mkdir npyImputeG2E_LB_2
-mkdir npyImputeR2E_LB_2
+mkdir npyImputeR2N_LK_2
+mkdir npyImputeF2E_LK_2
+mkdir npyImputeG1E_LK_2
 
 for i in {9..13}
 do
 sbatch run_experimentImpute_2_g_e_LK_$i.sh
 sleep 1
-sbatch run_experimentImpute_2_r_e_LK_$i.sh
+sbatch run_experimentImpute_2_g_n_LK_$i.sh
 sleep 1
-sbatch run_experimentImpute_2_g_e_LB_$i.sh
+sbatch run_experimentImpute_2_f_e_LK_$i.sh
 sleep 1
-sbatch run_experimentImpute_2_r_e_LB_$i.sh
+sbatch run_experimentImpute_1_g_e_LK_$i.sh
 sleep 1
 done
