739622fc72a3c57277aacc18502055384dcfec1d
juexinwang <juexinwang@hotmail.com>
2020-05-04 09:36:45 -0500

    celltype bug fix

--- submitCluster_celltype_5.sh
+++ submitCluster_celltype_5.sh
@@ -5,13 +5,13 @@ mkdir npyG1E_LK5
 
 for i in {9..13}
 do
-sbatch run_experimentImpute_2_g_e_LK_$i.sh
+sbatch run_experiment_2_g_e_LK_$i.sh
 sleep 1
-sbatch run_experimentImpute_2_g_f_LK_$i.sh
+sbatch run_experiment_2_g_f_LK_$i.sh
 sleep 1
-sbatch run_experimentImpute_2_n_e_LK_$i.sh
+sbatch run_experiment_2_n_e_LK_$i.sh
 sleep 1
-sbatch run_experimentImpute_1_g_e_LK_$i.sh
+sbatch run_experiment_1_g_e_LK_$i.sh
 sleep 1
 
 done
