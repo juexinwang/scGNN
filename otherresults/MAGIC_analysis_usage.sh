6b7fa9de6035c9660e59eb8d106eb3fe5ef3ca96
juexinwang <juexinwang@hotmail.com>
2020-05-04 16:00:30 -0500

    update

--- otherresults/MAGIC_analysis_usage.sh
+++ otherresults/MAGIC_analysis_usage.sh
@@ -1,17 +1,17 @@
-python MAGIC_impute_usage.py --datasetName 12.Klein --ratio 0.3 --replica 1
+python -W ignore MAGIC_impute_usage.py --datasetName 12.Klein --ratio 0.3 --replica 1
 
-python MAGIC_impute_usage.py --datasetName 12.Klein --ratio 0.3 --replica 2
+python -W ignore MAGIC_impute_usage.py --datasetName 12.Klein --ratio 0.3 --replica 2
 
-python MAGIC_impute_usage.py --datasetName 12.Klein --ratio 0.3 --replica 3
+python -W ignore MAGIC_impute_usage.py --datasetName 12.Klein --ratio 0.3 --replica 3
 
-python MAGIC_impute_usage.py --datasetName 12.Klein --ratio 0.6 --replica 1
+python -W ignore MAGIC_impute_usage.py --datasetName 12.Klein --ratio 0.6 --replica 1
 
-python MAGIC_impute_usage.py --datasetName 12.Klein --ratio 0.6 --replica 2
+python -W ignore MAGIC_impute_usage.py --datasetName 12.Klein --ratio 0.6 --replica 2
 
-python MAGIC_impute_usage.py --datasetName 12.Klein --ratio 0.6 --replica 3
+python -W ignore MAGIC_impute_usage.py --datasetName 12.Klein --ratio 0.6 --replica 3
 
-python MAGIC_impute_usage.py --datasetName 12.Klein --ratio 0.9 --replica 1
+python -W ignore MAGIC_impute_usage.py --datasetName 12.Klein --ratio 0.9 --replica 1
 
-python MAGIC_impute_usage.py --datasetName 12.Klein --ratio 0.9 --replica 2
+python -W ignore MAGIC_impute_usage.py --datasetName 12.Klein --ratio 0.9 --replica 2
 
-python MAGIC_impute_usage.py --datasetName 12.Klein --ratio 0.9 --replica 3
+python -W ignore MAGIC_impute_usage.py --datasetName 12.Klein --ratio 0.9 --replica 3
