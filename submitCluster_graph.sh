ee43d81659a342ca9d2665a69a0caf5af9ed03cf
juexinwang <juexinwang@hotmail.com>
2020-05-07 00:20:08 -0500

    fix typo

--- submitCluster_graph.sh
+++ submitCluster_graph.sh
@@ -14,21 +14,21 @@ for i in {9,11,12,13}
 do
 for j in {0..3}
 do
-sbatch run_experiment_1_n_e_LK_$i\_$j.sh
+sbatch run_experiment_1_g_e_LK_$i\_$j.sh
 sleep 1
 sbatch run_experiment_1_g_f_LK_$i\_$j.sh
 sleep 1
 sbatch run_experiment_1_n_e_LK_$i\_$j.sh
 sleep 1
 
-# sbatch run_experiment_1_n_e_LK2_$i\_$j.sh
+# sbatch run_experiment_1_g_e_LK2_$i\_$j.sh
 # sleep 1
 # sbatch run_experiment_1_g_f_LK2_$i\_$j.sh
 # sleep 1
 # sbatch run_experiment_1_n_e_LK2_$i\_$j.sh
 # sleep 1
 
-# sbatch run_experiment_1_n_e_LK3_$i\_$j.sh
+# sbatch run_experiment_1_g_e_LK3_$i\_$j.sh
 # sleep 1
 # sbatch run_experiment_1_g_f_LK3_$i\_$j.sh
 # sleep 1
