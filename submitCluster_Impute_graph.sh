a0c8f5cfba35dab0425fa77164c70e2a3c464ec8
juexinwang <juexinwang@hotmail.com>
2020-05-07 00:12:24 -0500

    add submit shell

---
+++ submitCluster_Impute_graph.sh
@@ -0,0 +1,38 @@
+mkdir npyImputeG1E_LK
+mkdir npyImputeG1F_LK
+mkdir npyImputeN1E_LK
+
+mkdir npyImputeG1E_LK_2
+mkdir npyImputeG1F_LK_2
+mkdir npyImputeN1E_LK_2
+
+mkdir npyImputeG1E_LK_3
+mkdir npyImputeG1F_LK_3
+mkdir npyImputeN1E_LK_3
+
+for i in {9,11,12,13}
+do
+for j in {0..3}
+sbatch run_experimentImpute_1_n_e_LK_$i\_$j.sh
+sleep 1
+sbatch run_experimentImpute_1_g_f_LK_$i\_$j.sh
+sleep 1
+sbatch run_experimentImpute_1_n_e_LK_$i\_$j.sh
+sleep 1
+
+# sbatch run_experimentImpute_1_n_e_LK2_$i\_$j.sh
+# sleep 1
+# sbatch run_experimentImpute_1_g_f_LK2_$i\_$j.sh
+# sleep 1
+# sbatch run_experimentImpute_1_n_e_LK2_$i\_$j.sh
+# sleep 1
+
+# sbatch run_experimentImpute_1_n_e_LK3_$i\_$j.sh
+# sleep 1
+# sbatch run_experimentImpute_1_g_f_LK3_$i\_$j.sh
+# sleep 1
+# sbatch run_experimentImpute_1_n_e_LK3_$i\_$j.sh
+# sleep 1
+done
+done
+
---
+++ submitCluster_graph.sh
@@ -0,0 +1,38 @@
+mkdir npyG1E_LK
+mkdir npyG1F_LK
+mkdir npyN1E_LK
+
+mkdir npyG1E_LK_2
+mkdir npyG1F_LK_2
+mkdir npyN1E_LK_2
+
+mkdir npyG1E_LK_3
+mkdir npyG1F_LK_3
+mkdir npyN1E_LK_3
+
+for i in {9,11,12,13}
+do
+for j in {0..3}
+sbatch run_experiment_1_n_e_LK_$i\_$j.sh
+sleep 1
+sbatch run_experiment_1_g_f_LK_$i\_$j.sh
+sleep 1
+sbatch run_experiment_1_n_e_LK_$i\_$j.sh
+sleep 1
+
+# sbatch run_experiment_1_n_e_LK2_$i\_$j.sh
+# sleep 1
+# sbatch run_experiment_1_g_f_LK2_$i\_$j.sh
+# sleep 1
+# sbatch run_experiment_1_n_e_LK2_$i\_$j.sh
+# sleep 1
+
+# sbatch run_experiment_1_n_e_LK3_$i\_$j.sh
+# sleep 1
+# sbatch run_experiment_1_g_f_LK3_$i\_$j.sh
+# sleep 1
+# sbatch run_experiment_1_n_e_LK3_$i\_$j.sh
+# sleep 1
+done
+done
+
