23bb56a88201394a15d2691a1582473f5f08b7bf
juexinwang <juexinwang@hotmail.com>
2020-05-11 10:47:49 -0500

    combine results in exploring together

--- results/results_impute_explore.sh
+++ results/results_impute_explore.sh
@@ -3,9 +3,9 @@ source activate conda_R
 dir='UaeC/'
 #$dir='UaeO/'
 for dataset in {'9.Chung','11.Kolodziejczyk','12.Klein','13.Zeisel'}
+do
 echo $dataset
 tdataset = $dataset | cut -d'.' -f 2
-do
 for i in {0.0,0.1,0.5,0.9,1.0}
 do
 for j in {1..3}
