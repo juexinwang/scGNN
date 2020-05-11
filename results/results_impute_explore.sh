d097e5065e0e27a16ad5f81c4a22745c87966b8d
juexinwang <juexinwang@hotmail.com>
2020-05-11 10:46:46 -0500

    combine results in exploring together

--- results/results_impute_explore.sh
+++ results/results_impute_explore.sh
@@ -1,10 +1,10 @@
 module load miniconda3
 source activate conda_R
-$dir='UaeC/'
+dir='UaeC/'
 #$dir='UaeO/'
 for dataset in {'9.Chung','11.Kolodziejczyk','12.Klein','13.Zeisel'}
 echo $dataset
-$tdataset = $dataset | cut -d'.' -f 2
+tdataset = $dataset | cut -d'.' -f 2
 do
 for i in {0.0,0.1,0.5,0.9,1.0}
 do
