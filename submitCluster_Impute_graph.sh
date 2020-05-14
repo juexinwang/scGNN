cbe96e308e6f90ff79b2b90b8e54b9e09683445f
juexinwang <juexinwang@hotmail.com>
2020-05-14 11:15:01 -0500

    update

--- submitCluster_Impute_graph.sh
+++ submitCluster_Impute_graph.sh
@@ -3,22 +3,22 @@ for i in {13..13}
 do
 for j in {0..27}
 do
-sbatch run_experimentImpute_1_g_e_LK_$i\_$j.sh
-sleep 1
+# sbatch run_experimentImpute_1_g_e_LK_$i\_$j.sh
+# sleep 1
 # sbatch run_experimentImpute_1_g_f_LK_$i\_$j.sh
 # sleep 1
 # sbatch run_experimentImpute_1_n_e_LK_$i\_$j.sh
 # sleep 1
 
-# sbatch run_experimentImpute_1_g_e_LK2_$i\_$j.sh
-# sleep 1
+sbatch run_experimentImpute_1_g_e_LK2_$i\_$j.sh
+sleep 1
 # sbatch run_experimentImpute_1_g_f_LK2_$i\_$j.sh
 # sleep 1
 # sbatch run_experimentImpute_1_n_e_LK2_$i\_$j.sh
 # sleep 1
 
-# sbatch run_experimentImpute_1_g_e_LK3_$i\_$j.sh
-# sleep 1
+sbatch run_experimentImpute_1_g_e_LK3_$i\_$j.sh
+sleep 1
 # sbatch run_experimentImpute_1_g_f_LK3_$i\_$j.sh
 # sleep 1
 # sbatch run_experimentImpute_1_n_e_LK3_$i\_$j.sh
