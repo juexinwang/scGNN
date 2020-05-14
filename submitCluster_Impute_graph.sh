c128c78d180120c2bebce2816082d460d0f54696
juexinwang <juexinwang@hotmail.com>
2020-05-14 11:18:37 -0500

    update

--- submitCluster_Impute_graph.sh
+++ submitCluster_Impute_graph.sh
@@ -5,24 +5,24 @@ for j in {0..27}
 do
 # sbatch run_experimentImpute_1_g_e_LK_$i\_$j.sh
 # sleep 1
-# sbatch run_experimentImpute_1_g_f_LK_$i\_$j.sh
-# sleep 1
-# sbatch run_experimentImpute_1_n_e_LK_$i\_$j.sh
-# sleep 1
-
-sbatch run_experimentImpute_1_g_e_LK2_$i\_$j.sh
+sbatch run_experimentImpute_1_g_f_LK_$i\_$j.sh
 sleep 1
-# sbatch run_experimentImpute_1_g_f_LK2_$i\_$j.sh
-# sleep 1
-# sbatch run_experimentImpute_1_n_e_LK2_$i\_$j.sh
-# sleep 1
-
-sbatch run_experimentImpute_1_g_e_LK3_$i\_$j.sh
+sbatch run_experimentImpute_1_n_e_LK_$i\_$j.sh
 sleep 1
-# sbatch run_experimentImpute_1_g_f_LK3_$i\_$j.sh
+
+# sbatch run_experimentImpute_1_g_e_LK2_$i\_$j.sh
 # sleep 1
-# sbatch run_experimentImpute_1_n_e_LK3_$i\_$j.sh
+sbatch run_experimentImpute_1_g_f_LK2_$i\_$j.sh
+sleep 1
+sbatch run_experimentImpute_1_n_e_LK2_$i\_$j.sh
+sleep 1
+
+# sbatch run_experimentImpute_1_g_e_LK3_$i\_$j.sh
 # sleep 1
+sbatch run_experimentImpute_1_g_f_LK3_$i\_$j.sh
+sleep 1
+sbatch run_experimentImpute_1_n_e_LK3_$i\_$j.sh
+sleep 1
 done
 done
 
