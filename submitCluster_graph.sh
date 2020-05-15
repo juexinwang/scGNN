8e31fe590d4ead63a1ad4ea42909d144a848253d
juexinwang <juexinwang@hotmail.com>
2020-05-15 16:25:59 -0500

    check clustering results

--- submitCluster_graph.sh
+++ submitCluster_graph.sh
@@ -1,18 +1,7 @@
-mkdir npyG1E_LK_1
-mkdir npyG1F_LK_1
-mkdir npyN1E_LK_1
-
-mkdir npyG1E_LK_2
-mkdir npyG1F_LK_2
-mkdir npyN1E_LK_2
-
-mkdir npyG1E_LK_3
-mkdir npyG1F_LK_3
-mkdir npyN1E_LK_3
-
-for i in {9,11,12,13}
+# for i in {9,11,12,13}
+for i in {13..13}
 do
-for j in {0..4}
+for j in {0..27}
 do
 sbatch run_experiment_1_g_e_LK_$i\_$j.sh
 sleep 1
@@ -21,19 +10,19 @@ sleep 1
 sbatch run_experiment_1_n_e_LK_$i\_$j.sh
 sleep 1
 
-# sbatch run_experiment_1_g_e_LK2_$i\_$j.sh
-# sleep 1
-# sbatch run_experiment_1_g_f_LK2_$i\_$j.sh
-# sleep 1
-# sbatch run_experiment_1_n_e_LK2_$i\_$j.sh
-# sleep 1
+sbatch run_experiment_1_g_e_LK2_$i\_$j.sh
+sleep 1
+sbatch run_experiment_1_g_f_LK2_$i\_$j.sh
+sleep 1
+sbatch run_experiment_1_n_e_LK2_$i\_$j.sh
+sleep 1
 
-# sbatch run_experiment_1_g_e_LK3_$i\_$j.sh
-# sleep 1
-# sbatch run_experiment_1_g_f_LK3_$i\_$j.sh
-# sleep 1
-# sbatch run_experiment_1_n_e_LK3_$i\_$j.sh
-# sleep 1
+sbatch run_experiment_1_g_e_LK3_$i\_$j.sh
+sleep 1
+sbatch run_experiment_1_g_f_LK3_$i\_$j.sh
+sleep 1
+sbatch run_experiment_1_n_e_LK3_$i\_$j.sh
+sleep 1
 done
 done
 
