3f7bf36ab431ef855f0b50c206f23a1029ea6e17
juexinwang <juexinwang@hotmail.com>
2020-05-15 16:47:06 -0500

    update for 3 possible results

--- submitCluster_Impute_graph.sh
+++ submitCluster_Impute_graph.sh
@@ -1,24 +1,24 @@
-# for i in {9,11,12,13}
-for i in {13..13}
+for i in {9,11,12}
+# for i in {13..13}
 do
-for j in {0..27}
+for j in {10,26,27}
 do
-# sbatch run_experimentImpute_1_g_e_LK_$i\_$j.sh
-# sleep 1
+sbatch run_experimentImpute_1_g_e_LK_$i\_$j.sh
+sleep 1
 sbatch run_experimentImpute_1_g_f_LK_$i\_$j.sh
 sleep 1
 sbatch run_experimentImpute_1_n_e_LK_$i\_$j.sh
 sleep 1
 
-# sbatch run_experimentImpute_1_g_e_LK2_$i\_$j.sh
-# sleep 1
+sbatch run_experimentImpute_1_g_e_LK2_$i\_$j.sh
+sleep 1
 sbatch run_experimentImpute_1_g_f_LK2_$i\_$j.sh
 sleep 1
 sbatch run_experimentImpute_1_n_e_LK2_$i\_$j.sh
 sleep 1
 
-# sbatch run_experimentImpute_1_g_e_LK3_$i\_$j.sh
-# sleep 1
+sbatch run_experimentImpute_1_g_e_LK3_$i\_$j.sh
+sleep 1
 sbatch run_experimentImpute_1_g_f_LK3_$i\_$j.sh
 sleep 1
 sbatch run_experimentImpute_1_n_e_LK3_$i\_$j.sh
--- submitCluster_graph.sh
+++ submitCluster_graph.sh
@@ -1,7 +1,8 @@
-# for i in {9,11,12,13}
-for i in {13..13}
+for i in {9,11,12}
+# for i in {13..13}
 do
-for j in {0..27}
+# for j in {0..27}
+for j in {10,26,27}
 do
 sbatch run_experiment_1_g_e_LK_$i\_$j.sh
 sleep 1
