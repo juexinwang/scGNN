0871f3cae5eda1e792090f85ffbb0e1211878906
juexinwang <juexinwang@hotmail.com>
2020-05-19 11:02:34 -0500

    celltype for onetime, immputation for three time

--- submitCluster_celltype.sh
+++ submitCluster_celltype.sh
@@ -3,15 +3,25 @@ mkdir npyG2F_LK_1
 mkdir npyN2E_LK_1
 mkdir npyG1E_LK_1
 
+# mkdir npyG2E_LK_2
+# mkdir npyG2F_LK_2
+# mkdir npyN2E_LK_2
+# mkdir npyG1E_LK_2
+
+# mkdir npyG2E_LK_3
+# mkdir npyG2F_LK_3
+# mkdir npyN2E_LK_3
+# mkdir npyG1E_LK_3
+
 for i in {9,11,12,13}
 do
-sbatch run_experiment_2_g_e_LK_$i.sh
+sbatch run_experiment_2_g_e_LK_1_$i.sh
 sleep 1
-sbatch run_experiment_2_g_f_LK_$i.sh
+sbatch run_experiment_2_g_f_LK_1_$i.sh
 sleep 1
-sbatch run_experiment_2_n_e_LK_$i.sh
+sbatch run_experiment_2_n_e_LK_1_$i.sh
 sleep 1
-sbatch run_experiment_1_g_e_LK_$i.sh
+sbatch run_experiment_1_g_e_LK_1_$i.sh
 sleep 1
 
 done
--- submitCluster_imputation_23.sh
+++ submitCluster_imputation_23.sh
@@ -1,7 +1,7 @@
-mkdir npyImputeG2E_LK
-mkdir npyImputeG2F_LK
-mkdir npyImputeN2E_LK
-mkdir npyImputeG1E_LK
+mkdir npyImputeG2E_LK_1
+mkdir npyImputeG2F_LK_1
+mkdir npyImputeN2E_LK_1
+mkdir npyImputeG1E_LK_1
 
 mkdir npyImputeG2E_LK_2
 mkdir npyImputeG2F_LK_2
@@ -15,13 +15,13 @@ mkdir npyImputeG1E_LK_3
 
 for i in {9,11,12,13}
 do
-sbatch run_experimentImpute_2_g_e_LK_$i.sh
+sbatch run_experimentImpute_2_g_e_LK_1_$i.sh
 sleep 1
-sbatch run_experimentImpute_2_g_f_LK_$i.sh
+sbatch run_experimentImpute_2_g_f_LK_1_$i.sh
 sleep 1
-sbatch run_experimentImpute_2_n_e_LK_$i.sh
+sbatch run_experimentImpute_2_n_e_LK_1_$i.sh
 sleep 1
-sbatch run_experimentImpute_1_g_e_LK_$i.sh
+sbatch run_experimentImpute_1_g_e_LK_1_$i.sh
 sleep 1
 
 sbatch run_experimentImpute_2_g_e_LK_2_$i.sh
