b12eff4d52b37d3a1a6d6f5a84d136529e441fab
juexinwang <juexinwang@hotmail.com>
2020-05-19 12:58:49 -0500

    single thread

--- submitCluster_Impute_graph.sh
+++ submitCluster_Impute_graph.sh
@@ -3,6 +3,8 @@ for i in {9,11,12,13}
 do
 for j in {0..27}
 do
+sbatch run_experimentImpute_2_g_e_LK_1_$i.sh
+sleep 1
 sbatch run_experimentImpute_1_g_e_LK_$i\_$j.sh
 sleep 1
 sbatch run_experimentImpute_1_g_f_LK_$i\_$j.sh
