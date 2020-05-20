0b97e6ac6c7ae129439fcbd38385fb3ef2a8854e
juexinwang <juexinwang@hotmail.com>
2020-05-20 10:00:06 -0500

    add seed 2 and seed 3 for celltype

--- submitCluster_celltype.sh
+++ submitCluster_celltype.sh
@@ -3,15 +3,15 @@ mkdir npyG2F_LK_1
 mkdir npyN2E_LK_1
 mkdir npyG1E_LK_1
 
-# mkdir npyG2E_LK_2
-# mkdir npyG2F_LK_2
-# mkdir npyN2E_LK_2
-# mkdir npyG1E_LK_2
+mkdir npyG2E_LK_2
+mkdir npyG2F_LK_2
+mkdir npyN2E_LK_2
+mkdir npyG1E_LK_2
 
-# mkdir npyG2E_LK_3
-# mkdir npyG2F_LK_3
-# mkdir npyN2E_LK_3
-# mkdir npyG1E_LK_3
+mkdir npyG2E_LK_3
+mkdir npyG2F_LK_3
+mkdir npyN2E_LK_3
+mkdir npyG1E_LK_3
 
 for i in {9,11,12,13}
 do
@@ -22,6 +22,24 @@ sleep 1
 sbatch run_experiment_2_n_e_LK_1_$i.sh
 sleep 1
 sbatch run_experiment_1_g_e_LK_1_$i.sh
+sleep 1
+
+sbatch run_experiment_2_g_e_LK_2_$i.sh
+sleep 1
+sbatch run_experiment_2_g_f_LK_2_$i.sh
+sleep 1
+sbatch run_experiment_2_n_e_LK_2_$i.sh
+sleep 1
+sbatch run_experiment_1_g_e_LK_2_$i.sh
+sleep 1
+
+sbatch run_experiment_2_g_e_LK_3_$i.sh
+sleep 1
+sbatch run_experiment_2_g_f_LK_3_$i.sh
+sleep 1
+sbatch run_experiment_2_n_e_LK_3_$i.sh
+sleep 1
+sbatch run_experiment_1_g_e_LK_3_$i.sh
 sleep 1
 
 done
