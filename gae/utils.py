827c52a3381dc41ea59f67dbc27df9255365cd53
juexinwang <juexinwang@hotmail.com>
2020-05-24 08:12:57 -0500

    update gae model with Double

--- gae/utils.py
+++ gae/utils.py
@@ -273,7 +273,7 @@ def preprocess_graph(adj):
 
 def sparse_mx_to_torch_sparse_tensor(sparse_mx):
     """Convert a scipy sparse matrix to a torch sparse tensor."""
-    sparse_mx = sparse_mx.tocoo().astype(np.float32)
+    sparse_mx = sparse_mx.tocoo().astype(np.float64)
     indices = torch.from_numpy(
         np.vstack((sparse_mx.row, sparse_mx.col)).astype(np.int64))
     values = torch.from_numpy(sparse_mx.data)
