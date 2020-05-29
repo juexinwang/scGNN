bc261583bd33dbcacffd3a24b9eff916206d9f4b
juexinwang <juexinwang@hotmail.com>
2020-05-29 17:02:36 -0500

    Test more dropout 0.3,0.6,0.8,0.9, results

--- results/results_impute_explore_0.3.sh
+++ results/results_impute_explore_0.3.sh
@@ -4,6 +4,6 @@ for ii in {'0.1','0.3','0.6','0.8','0.9'}
 do
 for j in {1..3}
 do
-python -W ignore results_impute_graph.py --datasetName 12.Klein --regulized-type LTMG  --benchmark --labelFilename /home/jwang/data/scData/12.Klein/Klein_cell_label.csv --regupara $para --ratio $ii --npyDir ../npyImputeG1E_LK_$j\/
+python -W ignore results_impute_graph.py --datasetName 12.Klein --regulized-type LTMG  --benchmark --labelFilename /home/jwang/data/scData/12.Klein/Klein_cell_label.csv --ratio $ii --npyDir ../npyImputeG1E_LK_$j\/
 done
 done
