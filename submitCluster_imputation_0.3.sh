a7bd5284ab560c56206aea77b068e7ba9e8ce679
juexinwang <juexinwang@hotmail.com>
2020-05-29 21:47:45 -0500

    grid of 0.1->0.8

--- submitCluster_imputation_0.3.sh
+++ submitCluster_imputation_0.3.sh
@@ -1,12 +1,22 @@
 mkdir npyImputeG2E_LK_1
 mkdir npyImputeG2E_LK_2
 mkdir npyImputeG2E_LK_3
+mkdir npyImputeG2E_LK_4
+mkdir npyImputeG2E_LK_5
+mkdir npyImputeG2E_LK_6
+mkdir npyImputeG2E_LK_7
+mkdir npyImputeG2E_LK_8
+mkdir npyImputeG2E_LK_9
+mkdir npyImputeG2E_LK_10
 
-for i in {1..3}
+for i in {1..10}
 do
-sbatch run_experimentImpute_2_g_e_LK_$i\_12.sh
+sbatch run_experimentImpute_2_g_e_LK_$i\_12_0.1.sh
+sbatch run_experimentImpute_2_g_e_LK_$i\_12_0.2.sh
 sbatch run_experimentImpute_2_g_e_LK_$i\_12_0.3.sh
+sbatch run_experimentImpute_2_g_e_LK_$i\_12_0.4.sh
+sbatch run_experimentImpute_2_g_e_LK_$i\_12_0.5.sh
 sbatch run_experimentImpute_2_g_e_LK_$i\_12_0.6.sh
+sbatch run_experimentImpute_2_g_e_LK_$i\_12_0.7.sh
 sbatch run_experimentImpute_2_g_e_LK_$i\_12_0.8.sh
-sbatch run_experimentImpute_2_g_e_LK_$i\_12_0.9.sh
 done
