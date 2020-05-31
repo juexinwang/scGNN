40d6c2071608c4710095e24510941d24b13183fb
juexinwang <juexinwang@hotmail.com>
2020-05-31 05:58:20 -0500

    change names for imputation results

---
+++ results/results_impute_imputation.sh
@@ -0,0 +1,11 @@
+module load miniconda3
+source activate conda_R
+for dataset in {'9.Chung','11.Kolodziejczyk','12.Klein','13.Zeisel'}
+do
+echo $dataset
+tdataset=$(echo $dataset | cut -d'.' -f 2)
+for j in {1..3}
+do
+python -W ignore results_impute_graph.py --datasetName $dataset --regulized-type LTMG  --benchmark --labelFilename /home/jwang/data/scData/$dataset\/$tdataset\_cell_label.csv --npyDir ../npyImputeG1E_LK_$j\/
+done
+done
