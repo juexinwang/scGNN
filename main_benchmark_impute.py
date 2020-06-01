c6e64f3f6d64125a73c5992559633684970ab567
juexinwang <juexinwang@hotmail.com>
2020-06-01 11:27:05 -0500

    re-evaluate imputation

--- main_benchmark_impute.py
+++ main_benchmark_impute.py
@@ -654,7 +654,7 @@ if __name__ == "__main__":
     #     model.load_state_dict(torch.load(ptfileEnd))
     
     # generate graph regularizer from graph
-    adj = adj.tolist() # Used for read/load
+    # adj = adj.tolist() # Used for read/load
     adjdense = sp.csr_matrix.todense(adj)
     adjsample = torch.from_numpy(adjdense)
     if args.precisionModel == 'Float':
