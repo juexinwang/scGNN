504edbf6735c0544b5e4c741cdf249cfe9221b0a
juexinwang <juexinwang@hotmail.com>
2020-06-18 01:11:19 -0500

    revisit L1 in imputation

--- submitCluster_impute_grid.sh
+++ submitCluster_impute_grid.sh
@@ -18,29 +18,29 @@ do
 for j in {0..27}
 do
 sbatch run_experimentImpute_2_g_e_LK1_$i\_$j.sh
-sleep 1
+# sleep 1
 sbatch run_experimentImpute_1_g_e_LK1_$i\_$j.sh
-sleep 1
+# sleep 1
 sbatch run_experimentImpute_2_g_f_LK1_$i\_$j.sh
-sleep 1
+# sleep 1
 sbatch run_experimentImpute_2_n_e_LK1_$i\_$j.sh
-sleep 1
+# sleep 1
 
 sbatch run_experimentImpute_2_g_e_LK2_$i\_$j.sh
-sleep 1
+# sleep 1
 sbatch run_experimentImpute_1_g_e_LK2_$i\_$j.sh
-sleep 1
+# sleep 1
 sbatch run_experimentImpute_2_g_f_LK2_$i\_$j.sh
-sleep 1
+# sleep 1
 sbatch run_experimentImpute_2_n_e_LK2_$i\_$j.sh
-sleep 1
+# sleep 1
 
 sbatch run_experimentImpute_2_g_e_LK3_$i\_$j.sh
-sleep 1
+# sleep 1
 sbatch run_experimentImpute_1_g_e_LK3_$i\_$j.sh
-sleep 1
+# sleep 1
 sbatch run_experimentImpute_2_g_f_LK3_$i\_$j.sh
-sleep 1
+# sleep 1
 sbatch run_experimentImpute_2_n_e_LK3_$i\_$j.sh
 sleep 1
 done
