be583819ed9a227e23327bc516e9e9d002643e91
juexinwang <juexinwang@hotmail.com>
2020-11-17 11:35:23 -0600

    add distribution sbatch file

---
+++ submitCluster_distribution.sh
@@ -0,0 +1,16 @@
+#submit plotting
+
+for i in {0.1,0.3,0.6,0.8}
+do
+sbatch plot_G2E_$i\_12.sh
+sbatch plot_G2EL_$i\_12.sh
+sbatch plot_G1E_$i\_12.sh
+sbatch plot_G2F_$i\_12.sh
+sbatch plot_N2E_$i\_12.sh
+
+sbatch plot_G2E_$i\_13.sh
+sbatch plot_G2EL_$i\_13.sh
+sbatch plot_G1E_$i\_13.sh
+sbatch plot_G2F_$i\_13.sh
+sbatch plot_N2E_$i\_13.sh
+done
