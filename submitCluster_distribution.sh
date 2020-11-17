c404291ad3f164d9a35346cf82b3e5c1c10e5a40
juexinwang <juexinwang@hotmail.com>
2020-11-17 17:35:21 -0600

    change orders

--- submitCluster_distribution.sh
+++ submitCluster_distribution.sh
@@ -3,12 +3,17 @@
 for i in {0.1,0.3,0.6,0.8}
 do
 sbatch plot_G2E_$i\_12.sh
+
+sbatch plot_G2E_$i\_13.sh
+done
+
+for i in {0.1,0.3,0.6,0.8}
+do
 sbatch plot_G2EL_$i\_12.sh
 sbatch plot_G1E_$i\_12.sh
 sbatch plot_G2F_$i\_12.sh
 sbatch plot_N2E_$i\_12.sh
 
-sbatch plot_G2E_$i\_13.sh
 sbatch plot_G2EL_$i\_13.sh
 sbatch plot_G1E_$i\_13.sh
 sbatch plot_G2F_$i\_13.sh
