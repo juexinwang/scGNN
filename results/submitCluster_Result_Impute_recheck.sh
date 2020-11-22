00129a4eae66402a8ce233ae0c1875d9410801b5
Wang <wangjue@umsystem.edu>
2020-11-22 10:43:21 -0600

    fix a bug

--- results/submitCluster_Result_Impute_recheck.sh
+++ results/submitCluster_Result_Impute_recheck.sh
@@ -2,16 +2,16 @@ for i in {0..59}
 do
 for j in {8,11,12,13}
 do
-python results_Reading_23dropout.py --methodName $i --splitMode --batchStr $j > run_Results_Impute_$i-$j.sh
+python results_Reading_recheck.py --methodName $i --splitMode --batchStr $j > run_Results_Impute_$i-$j.sh
 done
 done
 
 # submit
-for i in {0..59}
-do
-for j in {8,11,12,13}
-do
-sbatch run_Results_Impute_$i-$j.sh
-sleep 1
-done
-done
+# for i in {0..59}
+# do
+# for j in {8,11,12,13}
+# do
+# sbatch run_Results_Impute_$i-$j.sh
+# sleep 1
+# done
+# done
