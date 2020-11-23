9f7ae1e66e944e1210973cfd9ba6d1230e89435a
Wang <wangjue@umsystem.edu>
2020-11-23 15:40:53 -0600

    change ranking

--- results/submitCluster_Result_Impute_recheck.sh
+++ results/submitCluster_Result_Impute_recheck.sh
@@ -1,15 +1,15 @@
-for i in {0..59}
-do
-for j in {8,11,12,13}
-do
-python results_Reading_recheck.py --methodName $i --splitMode --batchStr $j > run_Results_Impute_$i-$j.sh
-done
-done
+# for i in {0..59}
+# do
+# for j in {8,11,12,13}
+# do
+# python results_Reading_recheck.py --methodName $i --splitMode --batchStr $j > run_Results_Impute_$i-$j.sh
+# done
+# done
 
 # submit
-for i in {0..59}
-do
 for j in {8,11,12,13}
+do
+for i in {0..59}
 do
 sbatch run_Results_Impute_$i-$j.sh
 sleep 1
