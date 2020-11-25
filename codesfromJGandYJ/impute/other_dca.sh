d8469d67c183170f0fe030bb39ef683b81f7cf3b
Wang <wangjue@umsystem.edu>
2020-11-25 10:28:41 -0600

    update saucie

--- codesfromJGandYJ/impute/other_dca.sh
+++ codesfromJGandYJ/impute/other_dca.sh
@@ -16,6 +16,6 @@
 #################################################################
 
 module load miniconda3
-source activate /storage/htc/joshilab/wangjue/conda_R_gpu
+source activate /storage/htc/joshilab/wangjue/conda_R_dca
 module load cuda/cuda-10.1.243
 python3 -W ignore dca_impute.py
