5fa98224abc05058b3a2f4b2063ff6c5761f55b3
Wang <wangjue@umsystem.edu>
2020-11-27 07:18:41 -0600

    update sbatch infor

--- results/submitCluster_Result_Impute_recheck.sh
+++ results/submitCluster_Result_Impute_recheck.sh
@@ -1,10 +1,10 @@
-# for i in {0..59}
-# do
-# for j in {9,11,12,13}
-# do
-# python results_Reading_recheck.py --methodName $i --splitMode --batchStr $j > run_Results_Impute_$i-$j.sh
-# done
-# done
+for i in {0..59}
+do
+for j in {9,11,12,13}
+do
+python results_Reading_recheck.py --methodName $i --splitMode --batchStr $j > run_Results_Impute_$i-$j.sh
+done
+done
 
 # submit
 for j in {9,11,12,13}
