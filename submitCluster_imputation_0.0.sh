9a27b23df948af3da2a2b94b44f84577820c52c5
Wang <wangjue@umsystem.edu>
2020-11-28 16:50:16 -0600

    ratio 0.0

--- submitCluster_imputation_0.0.sh
+++ submitCluster_imputation_0.0.sh
@@ -1,14 +1,4 @@
-for i in {1}
-do
-for j in {0.0}
-do
-sbatch run_experimentImpute_2_g_e_$i\_9_$j\.sh
-
-sbatch run_experimentImpute_2_g_e_$i\_11_$j\.sh
-
-sbatch run_experimentImpute_2_g_e_$i\_12_$j\.sh
-
-sbatch run_experimentImpute_2_g_e_$i\_13_$j\.sh
-
-done
-done
+sbatch run_experimentImpute_2_g_e_1_9_0.0.sh
+sbatch run_experimentImpute_2_g_e_1_11_0.0.sh
+sbatch run_experimentImpute_2_g_e_1_12_0.0.sh
+sbatch run_experimentImpute_2_g_e_1_13_0.0.sh
