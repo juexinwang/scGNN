cf553ba7cf738bdfa6993c83fbb9d0d5d4a8f4fa
Wang <wangjue@umsystem.edu>
2020-12-04 11:40:51 -0600

    only focus on distribution

--- submitCluster_distribution.sh
+++ submitCluster_distribution.sh
@@ -8,25 +8,25 @@ sbatch plot_G2E_$i\_12.sh
 sbatch plot_G2E_$i\_13.sh
 done
 
-for i in {0.1,0.3,0.6,0.8}
-do
-sbatch plot_G2EL_$i\_9.sh
-sbatch plot_G1E_$i\_9.sh
-sbatch plot_G2F_$i\_9.sh
-sbatch plot_N2E_$i\_9.sh
+# for i in {0.1,0.3,0.6,0.8}
+# do
+# sbatch plot_G2EL_$i\_9.sh
+# sbatch plot_G1E_$i\_9.sh
+# sbatch plot_G2F_$i\_9.sh
+# sbatch plot_N2E_$i\_9.sh
 
-sbatch plot_G2EL_$i\_11.sh
-sbatch plot_G1E_$i\_11.sh
-sbatch plot_G2F_$i\_11.sh
-sbatch plot_N2E_$i\_11.sh
+# sbatch plot_G2EL_$i\_11.sh
+# sbatch plot_G1E_$i\_11.sh
+# sbatch plot_G2F_$i\_11.sh
+# sbatch plot_N2E_$i\_11.sh
 
-sbatch plot_G2EL_$i\_12.sh
-sbatch plot_G1E_$i\_12.sh
-sbatch plot_G2F_$i\_12.sh
-sbatch plot_N2E_$i\_12.sh
+# sbatch plot_G2EL_$i\_12.sh
+# sbatch plot_G1E_$i\_12.sh
+# sbatch plot_G2F_$i\_12.sh
+# sbatch plot_N2E_$i\_12.sh
 
-sbatch plot_G2EL_$i\_13.sh
-sbatch plot_G1E_$i\_13.sh
-sbatch plot_G2F_$i\_13.sh
-sbatch plot_N2E_$i\_13.sh
-done
+# sbatch plot_G2EL_$i\_13.sh
+# sbatch plot_G1E_$i\_13.sh
+# sbatch plot_G2F_$i\_13.sh
+# sbatch plot_N2E_$i\_13.sh
+# done
